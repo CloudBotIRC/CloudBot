db124980e9eec6de8c952dfeb677643b8b89e711
Luke Rogers <lukeroge@gmail.com>
2011-11-21 00:10:16 +1300

    Updated bot useragent

--- plugins/util/http.py
+++ plugins/util/http.py
@@ -14,7 +14,7 @@ from urllib2 import HTTPError, URLError
 from lxml import etree, html
 
 
-ua_skybot = 'Cloudbot/3.4 http://github.com/lukeroge/cloudbot'
+ua_cloudbot = 'Cloudbot/DEV http://github.com/lukeroge/cloudbot'
 
 ua_firefox = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) ' \
     'Gecko/20070725 Firefox/2.0.0.6'
@@ -45,7 +45,7 @@ def open(url, query_params=None, user_agent=None, post_data=None,
         query_params = {}
 
     if user_agent is None:
-        user_agent = ua_skybot
+        user_agent = ua_cloudbot
 
     query_params.update(kwargs)
 

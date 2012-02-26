d5c81f6079ec98b98432e950421b99976cdd2e51
lukeroge <lukeroge@gmail.com>
2012-02-26 22:08:36 +1300

    Update plugins/shorten.py

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -2,14 +2,9 @@
 # <lukeroge@gmail.com> <https://github.com/lukeroge/CloudBot/>
 
 from util import hook, http
-
-try:
-  from re import match
-  from urllib2 import urlopen, Request, HTTPError
-  from urllib import urlencode
-  
-except ImportError, e:
-  raise Exception('Required module missing: %s' % e.args[0])
+from re import match
+from urllib2 import urlopen, Request, HTTPError
+from urllib import urlencode
 
 class ShortenError(Exception):
     def __init__(self, value):
@@ -18,20 +13,20 @@ class ShortenError(Exception):
         return repr(self.value)
  
 def bitly(url, user, apikey):
-  try:
-    params = urlencode({'longUrl': url, 'login': user, 'apiKey': apikey, 'format': 'json'})
-    j = http.get_json("http://api.bit.ly/v3/shorten?%s" % params)
-    if j['status_code'] == 200:
-      return j['data']['url']
-    raise ShortenError('%s'%j['status_txt'])
-  except (HTTPError, ShortenError):
-    return "Could not shorten %s!" % url
+    try:
+        params = urlencode({'longUrl': url, 'login': user, 'apiKey': apikey, 'format': 'json'})
+        j = http.get_json("http://api.bit.ly/v3/shorten?%s" % params)
+        if j['status_code'] == 200:
+            return j['data']['url']
+        raise ShortenError('%s'%j['status_txt'])
+    except (HTTPError, ShortenError):
+        return "Could not shorten %s!" % url
 
 @hook.command
 def shorten(inp, bot = None):
-  ".shorten <url> - Makes an j.mp/bit.ly shortlink to the url provided"
-  api_user = bot.config.get("api_keys", {}).get("bitly_user", None)    	
-  api_key = bot.config.get("api_keys", {}).get("bitly_api", None)
-  if api_key is None:
-     return "error: no api key set"
-  return bitly(inp, api_user, api_key)
+    ".shorten <url> - Makes an j.mp/bit.ly shortlink to the url provided"
+    api_user = bot.config.get("api_keys", {}).get("bitly_user", None)    	
+    api_key = bot.config.get("api_keys", {}).get("bitly_api", None)
+    if api_key is None:
+        return "error: no api key set"
+    return bitly(inp, api_user, api_key)

cbe02f005a3c8c450711127ddb920f39b95b24a5
neersighted <neersighted@neersighted.com>
2012-02-29 11:01:31 -0800

    Added config!

--- core/config.py
+++ core/config.py
@@ -41,6 +41,10 @@ if not os.path.exists('config'):
             "mc_user": "INSERT MINECRAFT USERNAME HERE",
             "mc_pass": "INSERT MINECRAFT PASSWORD HERE"
           },
+          "wrapper":
+          {
+            "backgrounder": "screen"
+          },
           "censored_strings":
           [
             "mypass",

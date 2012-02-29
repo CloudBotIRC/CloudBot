068ccab86bb1be4f5950c9881777e45c2d7378ab
neersighted <neersighted@neersighted.com>
2012-02-28 18:32:01 -0800

    moved ctcps arround

--- plugins/misc.py
+++ plugins/misc.py
@@ -55,20 +55,3 @@ def onjoin(paraml, conn=None, bot=None):
         while True:
             time.sleep(conn.conf.get('stayalive_delay', 20))
             conn.cmd('PING', [conn.nick])
-# CTCPs
-@hook.regex(r'^\x01VERSION\x01$')
-def ctcpversion(inp, notice=None):
-    notice('\x01VERSION: CloudBot - http://git.io/cloudbot')
-
-@hook.regex(r'^\x01PING\x01$')
-def ctcpping(inp, notice=None):
-    notice('\x01PING: PONG')
-
-@hook.regex(r'^\x01TIME\x01$')
-def ctcptime(inp, notice=None):
-    notice('\x01TIME: GET A WATCH')
-
-@hook.regex(r'^\x01FINGER\x01$')
-def ctcpfinger(inp, notice=None):
-    notice('\x01FINGER: WHERE ARE YOU PUTTING THAT')
-

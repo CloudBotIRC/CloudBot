dafe86fe2283e6d12a462cb5d45ba022060b0c1e
neersighted <neersighted@neersighted.com>
2012-03-01 14:24:25 -0800

    remove reference to wrapper in config

--- core/config.py
+++ core/config.py
@@ -41,10 +41,6 @@ if not os.path.exists('config'):
             "mc_user": "INSERT MINECRAFT USERNAME HERE",
             "mc_pass": "INSERT MINECRAFT PASSWORD HERE"
           },
-          "wrapper":
-          {
-            "backend": "daemon"
-          },
           "censored_strings":
           [
             "mypass",

5ffc63a7b4da5e731f43281e58a908545d1ee33f
neersighted <neersighted@neersighted.com>
2012-03-06 10:14:26 -0800

    Mereged kill, lart, and slap.py > violence.py

--- plugins/violence.py
+++ plugins/violence.py
@@ -2,6 +2,40 @@ from util import hook
 import re
 import random
 
+
+slaps = ["slaps <who> with a <item>.",
+        "slaps <who> around a bit with a <item>.",
+        "throws a <item> at <who>.",
+        "chucks a few <item>s at <who>.",
+        "grabs a <item> and throws it in <who>'s face.",
+        "launches a <item> in <who>'s general direction.",
+        "sits on <who>'s face, while slamming a <item> into their crotch.",
+        "holds <who> down and repeatedly whacks them with a <item>.",
+        "prods <who> with a flaming <item>.",
+        "picks up a <item>, and whacks <who> with it.",
+        "ties <who> to a chair and throws a <item> at them.",
+        "hits <who> on the head with a <item>."]
+
+items = ["cast iron skillet",
+        "large trout",
+        "baseball bat",
+        "wooden cane",
+        "CRT monitor",
+        "diamond sword",
+        "physics textbook",
+        "television",
+        "mau5head",
+        "five ton truck",
+        "roll of duct tape",
+        "book",
+        "cobblestone block",
+        "lava bucket",
+        "rubber chicken",
+        "gold block",
+        "fire extinguisher",
+        "heavy rock",
+        "chunk of dirt"]
+
 larts = ["swaps <who>'s shampoo with glue.",
         "installs Windows on <who>'s computer.",
         "forces <who> to use perl for 3 weeks.",
@@ -109,8 +143,57 @@ larts = ["swaps <who>'s shampoo with glue.",
         "assigns all of the permissions tickets on the BeastNode support system to <who>.",
         "hands <who> a poison ivy joint."]
 
+kills = ["rips off <who>'s <body> and leaves them to die.",
+        "grabs <who>'s head and rips it clean off their body.",
+        "grabs a machine gun and riddles <who>'s body with bullets.",
+        "sends The Terminator on a mission to retrieve <who>'s <body>.",
+        "gags and ties <who> then throws them off a bridge.",
+        "crushes <who> with a huge spiked boulder.",
+        "glares at <who> until they die of boredom.",
+        "stuffs a few TNT blocks under <who>'s bed, and sets them off.",
+        "shivs <who> in the <body>.",
+        "rams a rocket launcher up <who>'s ass and lets off a few rounds.",
+        "crushes <who>'s skull in with a spiked mace.",
+        "unleashes the armies of Isengard on <who>.",
+        "packs <who> into a SVN repo.",
+        "slices <who>'s <body> off with a Katana",
+        "throws <who> to Cthulu!",
+        "feeds <who> to an owlbear.",
+        "turns <who> into a snail, and then salts them.",
+        "snacks on <who>'s <body>.",
+        "puts <who> into a sack, throws the sack in the river, and hurls the river into space.",
+        "goes bowling with <who>'s head.",
+        "uses <who>'s <body> as a back-scratcher.",
+        "sends <who> to /dev/null!",
+        "feeds <who> coke and mentos till they pop!"]
+
+body = ['head',
+        'arms',
+        'leg',
+        'arm',
+        '"special parts"']
+
+
 @hook.command
-def lart(inp, me=None,  nick=None, input=None, notice=None):
+def slap(inp, me=None, nick=None, input=None, notice=None):
+    ".slap <user> -- Makes the bot slap <user>."
+    inp = inp.strip()
+
+    if not re.match("^[A-Za-z0-9_|.-\]\[]*$", inp.lower()):
+        notice("Invalid username!")
+        return
+
+    if inp == input.conn.nick.lower() or inp == "itself":
+        target = re.sub ('<who>', nick, random.choice(slaps))
+    else:
+        target = re.sub ('<who>', inp, random.choice(slaps))
+    msg = re.sub ('<item>', random.choice(items), target)
+    me(msg)
+
+
+
+@hook.command
+def lart(inp, me=None, nick=None, input=None, notice=None):
     ".lart <user> -- Makes the bot LART <user>."
     inp = inp.strip()
 
@@ -123,4 +206,24 @@ def lart(inp, me=None,  nick=None, input=None, notice=None):
     else:
         target = inp
     msg = re.sub ('<who>', target, random.choice(larts))
+    me(msg)
+
+
+
+@hook.command
+def kill(inp, me=None, nick=None, input=None, notice=None):
+    ".kill <user> -- Makes the bot kill <user>."
+    inp = inp.strip()
+
+    if not re.match("^[A-Za-z0-9_|.-\]\[]*$", inp.lower()):
+        notice("Invalid username!")
+        return
+
+    if inp == input.conn.nick.lower() or inp == "itself":
+        target = inp
+    else:
+        target = nick
+    msg = random.choice(kills)
+    msg = re.sub ('<who>', target, target)
+    msg = re.sub ('<body>', random.choice(body), target)
     me(msg)

22ba86ec5ae5a4fc05fd0a296dba46e0bfaa69f3
Luke Rogers <lukeroge@gmail.com>
2012-03-31 12:52:51 +1300

    You can now ignore by host

--- plugins/ignore.py
+++ plugins/ignore.py
@@ -30,7 +30,7 @@ def ignoresieve(bot, input, func, type, args):
     # don't block input to event hooks
     if type == "event":
         return input
-    if is_ignored(input.chan) or is_ignored(input.nick):
+    if is_ignored(input.chan) or is_ignored(input.nick) or is_ignored(input.host):
         if input.command == "PRIVMSG" and input.lastparam[1:] == "unignore":
             return input
         else:
@@ -40,16 +40,16 @@ def ignoresieve(bot, input, func, type, args):
 
 @hook.command(autohelp=False)
 def ignored(inp, bot=None):
-    ".ignored -- Lists ignored channels/users."
+    ".ignored -- Lists ignored channels/users/hosts."
     if ignorelist:
-        return "Ignored channels/users are: %s" % ", ".join(ignorelist)
+        return "Ignored channels/users/hosts are: %s" % ", ".join(ignorelist)
     else:
-        return "No channels/users are currently ignored."
+        return "No channels/users/hosts are currently ignored."
 
 
 @hook.command(adminonly=True)
 def ignore(inp, input=None, notice=None):
-    ".ignore <channel/user> -- Makes the bot ignore <channel/user>."
+    ".ignore <channels/users/hosts> -- Makes the bot ignore someone."
     target = inp
 
     if is_ignored(target):
@@ -61,7 +61,7 @@ def ignore(inp, input=None, notice=None):
 
 @hook.command(adminonly=True)
 def unignore(inp, input=None, notice=None):
-    ".unignore <channel/user> -- Makes the bot listen to <channel/user>."
+    ".unignore channels/users/hosts -- Makes the bot listen to someone."
     target = inp
 
     if is_ignored(target):

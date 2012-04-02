0b8f1f665d76fc26a8d448644a8d17361e3ab36d
neersighted <neersighted@neersighted.com>
2012-04-02 13:10:44 -0700

    Fixed spaceing in .me help message

--- plugins/admin.py
+++ plugins/admin.py
@@ -147,8 +147,8 @@ def say(inp, conn=None, chan=None, notice=None):
 @hook.command(adminonly=True)
 def me(inp, conn=None, chan=None, notice=None):
     ".me [channel] <action> -- Makes the bot act out <action> in [channel]. " \
-    "If [channel] is blank the bot will act the <action> in the channel the" \
-    " command was used in."
+    "If [channel] is blank the bot will act the <action> in the channel the " \
+    "command was used in."
     inp = inp.split(" ")
     if inp[0][0] == "#":
         message = ""

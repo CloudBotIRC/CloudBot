d2260bc5d85f04d47cc5d150b1e74f768fd8ccf6
lukeroge <lukeroge@gmail.com>
2012-04-18 16:35:57 +1200

    Fixed URL, small tweaks

--- plugins/drama.py
+++ plugins/drama.py
@@ -1,18 +1,17 @@
 from util import hook, http
 
-api_url = "http://encyclopediadramatica.ch/api.php?action=opensearch"
-ed_url = "http://encyclopediadramatica.ch/"
+api_url = "http://encyclopediadramatica.se/api.php?action=opensearch"
+ed_url = "http://encyclopediadramatica.se/"
 
 
-@hook.command('ed')
 @hook.command
 def drama(inp):
-    ".drama <phrase> -- Gets the first paragraph of"\
-    "the Encyclopedia Dramatica article on <phrase>."
+    ".drama <phrase> -- Gets the first paragraph of" \
+    " the Encyclopedia Dramatica article on <phrase>."
 
     j = http.get_json(api_url, search=inp)
     if not j[1]:
-        return 'no results found'
+        return "No results found."
     article_name = j[1][0].replace(' ', '_').encode('utf8')
 
     url = ed_url + http.quote(article_name, '')
@@ -20,9 +19,9 @@ def drama(inp):
 
     for p in page.xpath('//div[@id="bodyContent"]/p'):
         if p.text_content():
-            summary = ' '.join(p.text_content().splitlines())
+            summary = " ".join(p.text_content().splitlines())
             if len(summary) > 300:
                 summary = summary[:summary.rfind(' ', 0, 300)] + "..."
-            return '%s :: \x02%s\x02' % (summary, url)
+            return "%s :: \x02%s\x02" % (summary, url)
 
-    return "error"
+    return "Unknown Error."

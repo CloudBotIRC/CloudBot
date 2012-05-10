d5ba3e2e560965e5ceab787a11fb80708d080e19
Luke Rogers <lukeroge@gmail.com>
2012-05-11 07:40:27 +1200

    Tweaked hash command

--- plugins/hash.py
+++ plugins/hash.py
@@ -30,4 +30,4 @@ def sha512(inp):
 def hash(inp):
     ".hash <text> -- Returns hashes of <text>."
     return ', '.join(x + ": " + getattr(hashlib, x)(inp).hexdigest()
-            for x in 'md5 sha1 sha256'.split())
+            for x in ['md5', 'sha1', 'sha256'])

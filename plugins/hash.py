e8e7c297ef992d4c5e5253a0dcf1accc4c304ce8
Luke Rogers <lukeroge@gmail.com>
2012-05-17 07:45:16 +1200

    Bug fix

--- plugins/hash.py
+++ plugins/hash.py
@@ -28,6 +28,6 @@ def sha512(inp):
 
 @hook.command
 def hash(inp):
-    .hash <text> -- Returns hashes of <text>."
+    "hash <text> -- Returns hashes of <text>."
     return ', '.join(x + ": " + getattr(hashlib, x)(inp).hexdigest()
             for x in ['md5', 'sha1', 'sha256'])

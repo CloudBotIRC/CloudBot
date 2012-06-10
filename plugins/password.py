a47d99c0c22cc86c465165d48b864bf95af8cb1d
Luke Rogers <lukeroge@gmail.com>
2012-06-11 10:21:44 +1200

    Update develop

--- plugins/password.py
+++ plugins/password.py
@@ -10,40 +10,35 @@ def gen_password(types):
     okay = []
     #find the length needed for the password
     numb = types.split(" ")
-
-    for x in numb[0]:
-        #if any errors are found defualt to 10
-        if x not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            numb[0] = 10
-    length = int(numb[0])
+    
+    try:
+        length = int(numb[0])
+    except ValueError:
+        length = 10
+        
     needs_def = 0
     #alpha characters
     if "alpha" in types or "letter" in types:
-        for x in string.ascii_lowercase:
-            okay.append(x)
+        okay = okay + string.ascii_lowercase
         #adds capital characters if not told not to
         if "no caps" not in types:
-            for x in string.ascii_uppercase:
-                okay.append(x)
+            okay = okay + string.ascii_uppercase
     else:
         needs_def = 1
     #adds numbers
     if "numeric" in types or "numbers" in types:
-        for x in range(0, 10):
-            okay.append(str(x))
+        okay = okay + [str(x) for x in range(0, 10)]
     else:
         needs_def = 1
     #adds symbols
     if "symbols" in types:
         sym = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '[', ']', '{', '}', '\\', '|', ';', ':', "'", '.', '>', ',', '<', '/', '?', '`', '~', '"']
-        for x in sym:
-            okay.append(x)
+        okay = okay + sym
     else:
         needs_def = 1
     #defaults to lowercase alpha password if no arguments are found
     if needs_def == 1:
-        for x in string.ascii_lowercase:
-            okay.append(x)
+        okay = okay + [str(x) for x in xrange(0, 10)]
     password = ""
     #generates password
     for x in range(length):

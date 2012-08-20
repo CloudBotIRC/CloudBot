7bdbb376b8f802cd7b480c565305c52c72dc3e8b
Luke Rogers <lukeroge@gmail.com>
2012-08-21 07:49:22 +1200

    Pepping

--- plugins/spellcheck.py
+++ plugins/spellcheck.py
@@ -8,23 +8,21 @@ locale = "en_US"
 @hook.command("spellcheck")
 def spell(inp):
     "spell <word> -- Check spelling of <word>."
-    word = inp
-	
-    if ' ' in word:
+
+    if ' ' in inp:
         return "This command only supports one word at a time."
 
     if not enchant.dict_exists(locale):
         return "Could not find dictionary: %s" % locale
 
     dict = enchant.Dict(locale)
-
-    is_correct = dict.check(word)
-    suggestions = dict.suggest(word)
+    is_correct = dict.check(inp)
+    suggestions = dict.suggest(inp)
     s_string = ', '.join(suggestions[:10])
 
     if is_correct:
         return '"%s" appears to be \x02valid\x02! ' \
-               '(suggestions: %s)' % (word, s_string)
+               '(suggestions: %s)' % (inp, s_string)
     else:
         return '"%s" appears to be \x02invalid\x02! ' \
-               '(suggestions: %s)' % (word, s_string)
+               '(suggestions: %s)' % (inp, s_string)

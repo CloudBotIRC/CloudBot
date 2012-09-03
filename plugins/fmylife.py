6b4042972b38d53c5a6b7d44d5721e5dbc1aa1c5
Luke Rogers <lukeroge@gmail.com>
2012-09-04 09:09:57 +1200

    Testing the ability to include libraries with the bot

--- plugins/fmylife.py
+++ plugins/fmylife.py
@@ -1,24 +1,20 @@
-# Plugin by Lukeroge
-
 from util import hook, http
-from BeautifulSoup import BeautifulSoup
-from collections import defaultdict
 
-fml_cache = defaultdict()
+from bs4 import BeautifulSoup
+
+fml_cache = []
 
 
 def refresh_cache():
     """ gets a page of random FMLs and puts them into a dictionary """
     page = http.get('http://www.fmylife.com/random/')
-    soup = BeautifulSoup(page)
+    soup = BeautifulSoup(page, 'lxml')
 
-    for e in soup.findAll('div', {'class': 'post article'}):
+    for e in soup.find_all('div', {'class': 'post article'}):
         id = int(e['id'])
-        # get the text of the FML
-        text = ''.join(e.find('p').findAll(text=True))
+        text = ''.join(e.find('p').find_all(text=True))
         text = http.unescape(text)
-        # append to the dictionary
-        fml_cache[id] = text
+        fml_cache.append((id, text))
 
 # do an initial refresh of the cache
 refresh_cache()
@@ -29,7 +25,7 @@ def fml(inp, reply=None):
     "fml -- Gets a random quote from fmyfife.com."
 
     # grab the last item in the fml cache and remove it
-    id, text = fml_cache.popitem()
+    id, text = fml_cache.pop()
     # reply with the fml we grabbed
     reply('(#%d) %s' % (id, text))
     # refresh fml cache if its getting empty

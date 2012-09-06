8980610aa940361a6777cfcfbb079b76c94b73e3
Luke Rogers <lukeroge@gmail.com>
2012-09-07 10:50:25 +1200

    Update plugins/coin.py

--- plugins/coin.py
+++ plugins/coin.py
@@ -1,27 +1,26 @@
 from util import hook
-from random import getrandbits, normalvariate
+import random
 
 
 @hook.command(autohelp=False)
 def coin(inp, me=None):
     "coin [amount] -- Flips [amount] of coins."
 
-    if inp.isdigit():
+    try:
         amount = int(inp)
-    else:
-        amount = 1
+    except ValueError:
+        return "Invalid input!"
 
-    if amount > 90001:
-        return "Too many coins! Maximum is 90001."
-    elif amount == 1:
+    if amount == 1:
         if getrandbits(1):
             me("flips a coin and gets heads.")
         else:
             me("flips a coin and gets tails.")
+        me("flips a coin and gets %s." % random.choice(["heads", "tails"]))
     elif amount == 0:
         me("makes a coin flipping motion with its hands.")
     else:
-        heads = int(normalvariate(.5 * amount, (.75 * amount) ** .5))
+        heads = int(random.normalvariate(.5 * amount, (.75 * amount) ** .5))
         tails = amount - heads
         me("flips %i coins and gets " \
         "%i heads and %i tails." % (amount, heads, tails))

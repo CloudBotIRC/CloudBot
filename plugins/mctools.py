04d1de1ac8bd8da12bc7473f042d2899e6430771
Luke Rogers <lukeroge@gmail.com>
2012-10-01 21:31:08 +1300

    removed skin server check

--- plugins/mctools.py
+++ plugins/mctools.py
@@ -54,13 +54,6 @@ def mcstatus(inp, say=None):
     # change the json from a list of dictionaies to a dictionary
     data = json.loads(request.replace("}", "").replace("{", "").replace("]", "}").replace("[", "{"))
 
-    # check if skins are online
-    try:
-        request = http.get("http://s3.amazonaws.com/MinecraftSkins/Shnaw.png")
-        data["minecraft skin server"] = "green"
-    except (http.URLError, http.HTTPError) as e:
-        data["minecraft skin server"] = "red"
-
     out = []
     # use a loop so we don't have to update it if they add more servers
     for server, status in data.items():

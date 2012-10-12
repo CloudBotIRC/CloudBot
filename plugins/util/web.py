b6114a8617627b384df6077c28a950a846d12f2e
Luke Rogers <lukeroge@gmail.com>
2012-10-12 23:08:03 +1300

    tweak

--- plugins/util/web.py
+++ plugins/util/web.py
@@ -2,14 +2,23 @@
 
 import http, urlnorm
 import json, urllib
+import yql
 
 short_url = "http://is.gd/create.php"
 paste_url = "http://paste.dmptr.com"
+yql_env = "http://datatables.org/alltables.env"
+
+YQL = yql.Public()
+
+
+def query(query, params={}):
+    """ runs a YQL query and returns the results """
+    return YQL.execute(query, params, env=yql_env)
 
 
 def isgd(url):
     """ shortens a URL with the is.gd PAI """
-    url = urlnorm.normalize(url.encode('utf-8'))
+    url = urlnorm.normalize(url.encode('utf-8'), assume_scheme='http')
     params = urllib.urlencode({'format': 'simple', 'url': url})
     return http.get("http://is.gd/create.php?%s" % params)
 

9185e7c8d5dc50925db86c28893ed3f1ea2b4f88
Luke Rogers <lukeroge@gmail.com>
2013-02-06 19:34:01 +1300

    Added support for Spotify album and artist links.

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -11,7 +11,13 @@ spotify_re = (r'(open\.spotify\.com\/(track|album|artist|user)\/'
 @hook.regex(*spotify_re)
 def spotify_url(match):
     spotify_type = match.group(2)
+    print spotify_type
     spotify_id = match.group(3)
     url = gateway.format(spotify_type, spotify_id)
-    track = spotimeta.lookup(url)
-    return "{} - {}".format(track["result"]["artist"]["name"], track["result"]["name"])
+    data = spotimeta.lookup(url)
+    if spotify_type == "track":
+        return "Spotify Track: {} - {}".format(data["result"]["artist"]["name"], data["result"]["name"])
+    elif spotify_type == "artist":
+        return "Spotify Artist: {}".format(data["result"]["name"])
+    elif spotify_type == "album":
+        return "Spotify Album: {} - {}".format(data["result"]["artist"]["name"], data["result"]["name"])

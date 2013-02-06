1ea59ad7ad49902de1a4c30d7258eef5623af17b
Luke Rogers <lukeroge@gmail.com>
2013-02-07 01:03:18 +1300

    Added basic Yahoo answers plugin.

---
+++ plugins/answers.py
@@ -0,0 +1,14 @@
+from util import hook, web, text
+
+
+@hook.command
+def answer(inp):
+    ".answer <query> -- find the answer to a question on Yahoo! Answers"
+
+    query = "SELECT Subject, ChosenAnswer, Link FROM answers.search WHERE query=@query LIMIT 1"
+    result = web.query(query, {"query": inp.strip()}).one()
+
+    short_url = web.isgd(result["Link"])
+    answer = text.truncate_str(result["ChosenAnswer"], 80)
+
+    return u"{} {} - {}".format(result["Subject"], answer, short_url)

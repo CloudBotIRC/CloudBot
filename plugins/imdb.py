9779943b910fc5ad64669fdb5463fdaca85922b1
Luke Rogers <lukeroge@gmail.com>
2013-07-02 05:14:54 +1200

    Tweaked IMDB.py a bit

--- plugins/imdb.py
+++ plugins/imdb.py
@@ -1,26 +1,11 @@
 # IMDb lookup plugin by Ghetto Wizard (2011) and blha303 (2013)
 
-from util import hook, http
+from util import hook, http, text
 import re
-import json
 
 id_re = re.compile("tt\d+")
+imdb_re = (r'(.*:)//(imdb.com|www.imdb.com)(:[0-9]+)?(.*)', re.I)
 
-def truncate(msg):
-    nmsg = msg.split(" ")
-    out = None
-    x = 0
-    for i in nmsg:
-      if x <= 7:
-        if out:
-          out = out + " " + nmsg[x]
-        else:
-          out = nmsg[x]
-      x = x + 1
-    if x <= 7:
-      return out
-    else:
-      return out + "..."
 
 @hook.command
 def imdb(inp):
@@ -49,19 +34,18 @@ def imdb(inp):
     else:
         return 'Unknown error.'
 
-imdb_re = (r'(.*:)//(imdb.com|www.imdb.com)(:[0-9]+)?(.*)', re.I)
 
 @hook.regex(*imdb_re)
 def imdb_url(match):
     id = match.group(4).split('/')[-1]
     if id == "":
-      id = match.group(4).split('/')[-2]
+        id = match.group(4).split('/')[-2]
     content = http.get_json("http://www.omdbapi.com/", i=id)
     if content.get('Error', None) == 'Movie not found!':
         return 'Movie not found!'
     elif content['Response'] == 'True':
         content['URL'] = 'http://www.imdb.com/title/%(imdbID)s' % content
-        content['Plot'] = truncate(content['Plot'])
+        content['Plot'] = text.truncate_str(content['Plot'], 50)
         out = '\x02%(Title)s\x02 (%(Year)s) (%(Genre)s): %(Plot)s'
         if content['Runtime'] != 'N/A':
             out += ' \x02%(Runtime)s\x02.'

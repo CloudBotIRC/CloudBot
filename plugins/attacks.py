d69feb68d85f4268e84a5f031bf8215cb6015d63
Luke Rogers <lukeroge@gmail.com>
2013-08-01 10:36:47 +1200

    merged feelings.py and violence.py

--- plugins/attacks.py
+++ plugins/attacks.py
@@ -17,6 +17,14 @@ with open("plugins/data/kills.txt") as f:
     kills = [line.strip() for line in f.readlines()
              if not line.startswith("//")]
 
+with open("plugins/data/insults.txt") as f:
+    insults = [line.strip() for line in f.readlines()
+               if not line.startswith("//")]
+
+with open("plugins/data/flirts.txt") as f:
+    flirts = [line.strip() for line in f.readlines()
+              if not line.startswith("//")]
+
 
 @hook.command
 def slap(inp, me=None, nick=None, conn=None, notice=None):
@@ -76,3 +84,39 @@ def kill(inp, me=None, nick=None, conn=None, notice=None):
 
     # act out the message
     me(phrase.format(**values))
+
+
+@hook.command
+def insult(inp, nick=None, me=None, conn=None, notice=None):
+    "insult <user> -- Makes the bot insult <user>."
+    target = inp.strip()
+
+    if " " in target:
+        notice("Invalid username!")
+        return
+
+    if target == conn.nick.lower() or target == "itself":
+        target = nick
+    else:
+        target = inp
+
+    out = 'insults %s... "%s"' % (target, random.choice(insults))
+    me(out)
+
+
+@hook.command
+def flirt(inp, nick=None, me=None, conn=None, notice=None):
+    "flirt <user> -- Make the bot flirt with <user>."
+    target = inp.strip()
+
+    if " " in target:
+        notice("Invalid username!")
+        return
+
+    if target == conn.nick.lower() or target == "itself":
+        target = 'itself'
+    else:
+        target = inp
+
+    out = 'flirts with %s... "%s"' % (target, random.choice(flirts))
+    me(out)

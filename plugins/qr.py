41fe6723fc5bcf8bc0c7e48e559cd0c9dd13c5dd
Nathan Blaney <nathanblaney98@gmail.com>
2013-08-05 20:56:46 +1000

    Updated to use proper shorten

--- plugins/qr.py
+++ plugins/qr.py
@@ -8,8 +8,7 @@ def qrcode(inp, bot=None):
     "qrcode [link] returns a link for a QR code."
 
     link = "http://blny.tk/qr.php?q=%s" % http.quote_plus(inp)
+    
+    return web.try_isgd(link)
 
-    try:
-        return web.try_isgd(link)
-    except (web.ShortenError, http.HTTPError):
-        return link
+

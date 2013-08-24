9356e4e9ca86785f245b5c81639ecc76c8592368
Steven Smith <stevensmith.ome@gmail.com>
2013-08-24 16:44:22 +0800

    Fix duration, add new function for timestamp formatting

--- plugins/vimeo.py
+++ plugins/vimeo.py
@@ -1,15 +1,21 @@
-from util import hook, http
+from util import hook, http, timeformat
 
 
 @hook.regex(r'vimeo.com/([0-9]+)')
 def vimeo_url(match):
     "vimeo <url> -- returns information on the Vimeo video at <url>"
-    info = http.get_json('http://vimeo.com/api/v2/video/%s.json'
-                         % match.group(1))
+    info = http.get_json('http://vimeo.com/api/v2/video/%s.json' % match.group(1))
 
     if info:
-        return ("\x02%(title)s\x02 - length \x02%(duration)ss\x02 - "
-                "\x02%(stats_number_of_likes)s\x02 likes - "
-                "\x02%(stats_number_of_plays)s\x02 plays - "
-                "\x02%(user_name)s\x02 on \x02%(upload_date)s\x02"
-                % info[0])
+        info = info[0]
+        return ("\x02{title}\x02 - length \x02{duration}\x02 - "
+                "\x02{likes}\x02 likes - "
+                "\x02{plays}\x02 plays - "
+                "\x02{username}\x02 on \x02{uploaddate}\x02".format(
+                    title=info["title"],
+                    duration=timeformat.timeformat(info["duration"]),
+                    likes=info["stats_number_of_likes"],
+                    plays=info["stats_number_of_plays"],
+                    username=info["user_name"],
+                    uploaddate=info["upload_date"])
+               )

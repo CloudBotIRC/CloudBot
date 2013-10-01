8eff33b1f77a720c21d37644b106522aac6d71f2
Luke Rogers <lukeroge@gmail.com>
2013-10-01 19:22:59 +1300

    @cybojenix this

--- core/config.py
+++ core/config.py
@@ -4,13 +4,13 @@ import os
 
 class Config(dict):
     def __init__(self, name, *args, **kwargs):
-        self.path = os.path.abspath(name)
+        self.path = os.path.abspath("{}.json".format(name))
         self.update(*args, **kwargs)
 
     def reload(self):
         with open(self.path) as f:
             self = json.load(f)
-            print self
+        print self
 
 
     def save(self):

0719e86c5afd4a8355cc1acd7e33de244496031d
Luke Rogers <lukeroge@gmail.com>
2013-10-03 22:15:48 +1300

    this never happened

--- cloudbot.py
+++ cloudbot.py
@@ -8,6 +8,10 @@ import signal
 # set up enviroment
 os.chdir(sys.path[0] or '.')  # do stuff relative to the install directory
 
+# this is not the code you are looking for
+if os.path.exists(os.path.abspath('lib')):
+    sys.path += ['lib'] 
+
 print 'CloudBot2 <http://git.io/cloudbotirc>'
 
 def exit_gracefully(signum, frame):

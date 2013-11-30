77459a0879a2b66aa3dbb3bab3d421b3a0759460
Luke Rogers <lukeroge@gmail.com>
2013-12-01 00:49:09 +1300

    plurals!

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -20,25 +20,25 @@ def plural(num=0, text=''):
 def format_time(seconds, accuracy=3, simple=False):
     if simple:
         periods = [
-                ('c', 60*60*24*365*100),
-                ('de', 60*60*24*365*10),
-                ('y', 60*60*24*365),
-                ('m', 60*60*24*30),
-                ('d', 60*60*24),
-                ('h', 60*60),
+                ('c', 60 * 60 * 24 * 365 * 100),
+                ('de', 60 * 60 * 24 * 365 * 10),
+                ('y', 60 * 60 * 24 * 365),
+                ('m', 60 * 60 * 24 * 30),
+                ('d', 60 * 60 * 24),
+                ('h', 60 * 60),
                 ('m', 60),
                 ('s', 1)
                 ]
     else:
         periods = [
-                (' century', 60*60*24*365*100),
-                (' decade', 60*60*24*365*10),
-                (' year', 60*60*24*365),
-                (' month', 60*60*24*30),
-                (' day', 60*60*24),
-                (' hour', 60*60),
-                (' minute', 60),
-                (' second', 1)
+                (('century', 'centuries'), 60 * 60 * 24 * 365 * 100),
+                (('decade', 'decades'), 60 * 60 * 24 * 365 * 10),
+                (('year', 'years'), 60 * 60 * 24 * 365),
+                (('month', 'months'), 60 * 60 * 24 * 30),
+                (('day', 'days'), 60 * 60 * 24),
+                (('hour', 'hours'), 60 * 60),
+                (('minute', 'minutes'), 60),
+                (('second', 'seconds'), 1)
                 ]
 
     strings = []
@@ -48,10 +48,13 @@ def format_time(seconds, accuracy=3, simple=False):
             if seconds > period_seconds:
                     period_value, seconds = divmod(seconds,period_seconds)
                     i += 1
-                    if period_value == 1 or simple:
-                            strings.append("{}{}".format(period_value, period_name))
+                    if simple:
+                        strings.append("{}{}".format(period_value, period_name))
                     else:
-                            strings.append("{}{}s".format(period_value, period_name))
+                        if period_value == 1:
+                            strings.append("{} {}".format(period_value, period_name[0]))
+                        else:
+                            strings.append("{} {}".format(period_value, period_name[1]))
         else:
             break
 

402ecc0113e417b5abd009a6fd41d0d70006eb0a
Dabo Ross <daboross@daboross.net>
2014-04-05 23:14:12 -0700

    Fix correction plugin / resolve conflicts

--- plugins/correction.py
+++ plugins/correction.py
@@ -1,32 +1,5 @@
 from util import hook
 
-import re
-
-<<<<<<< HEAD
-@hook.regex(r'^(s|S)/.*/.*/\S*$')
-def correction(inp, message=None, input=None, notice=None, db=None):
-    splitinput = input.msg.split("/")
-    if splitinput[3]:
-        nick = splitinput[3]
-    else:
-        nick = input.nick
-    last_message = db.execute("select name, quote from seen_user where name"
-                              " like :nick and chan = :chan", {'nick': nick.lower(),
-                                                               'chan': input.chan.lower()}).fetchone()
-
-    if last_message:
-        splitinput = input.msg.split("/")
-        find = splitinput[1]
-        replace = splitinput[2]
-        if find in last_message[1]:
-            if "\x01ACTION" in last_message[1]:
-                msg = last_message[1].replace("\x01ACTION ", "/me ").replace("\x01", "")
-            else:
-                msg = last_message[1]
-            message(u"Correction, <{}> {}".format(nick, msg.replace(find, "\x02" + replace + "\x02")))
-        else:
-            notice(u"{} can't be found in your last message".format(find))
-=======
 CORRECTION_RE = r'^(s|S)/.*/.*/?\S*$'
 
 
@@ -36,7 +9,6 @@ def correction(match, input=None, conn=None, message=None):
 
     if len(split) == 4:
         nick = split[3].lower()
->>>>>>> develop
     else:
         nick = None
 
@@ -54,10 +26,9 @@ def correction(match, input=None, conn=None, message=None):
         if find in msg:
             if "\x01ACTION" in msg:
                 msg = msg.replace("\x01ACTION ", "/me ").replace("\x01", "")
-            message(u"Correction, <{}> {}".format(name, msg.replace(find, "\x02" + replace + "\x02")))
+            message("Correction, <{}> {}".format(name, msg.replace(find, "\x02" + replace + "\x02")))
             return
         else:
             continue
 
-    return u"Did not find {} in any recent messages.".format(find)
-
+    return "Did not find {} in any recent messages.".format(find)

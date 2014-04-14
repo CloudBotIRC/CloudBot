469dee90a5bce32858456ccbfe36de5698acca9c
Dabo Ross <daboross@daboross.net>
2014-04-13 17:03:39 -0700

    Move down and isup into one 'pagecheck' plugin. Also fix both of them for python 3

--- plugins/pagecheck.py
+++ plugins/pagecheck.py
@@ -4,20 +4,39 @@ from util import hook, http, urlnorm
 
 
 @hook.command
+def down(inp):
+    """down <url> -- Checks if the site at <url> is up or down.
+    :type inp: str
+    """
+
+    if not inp.startswith("http://"):
+        inp = 'http://' + inp
+
+    inp = 'http://' + urllib.parse.urlparse(inp).netloc
+
+    try:
+        http.get(inp, get_method='HEAD')
+        return '{} seems to be up'.format(inp)
+    except http.URLError:
+        return '{} seems to be down'.format(inp)
+
+
+@hook.command
 def isup(inp):
-    """isup -- uses isup.me to see if a site is up or not"""
+    """isup -- uses isup.me to see if a site is up or not
+    :type inp: str
+    """
 
     # slightly overcomplicated, esoteric URL parsing
     scheme, auth, path, query, fragment = urllib.parse.urlsplit(inp.strip())
 
-    domain = auth.encode('utf-8') or path.encode('utf-8')
+    domain = auth or path
     url = urlnorm.normalize(domain, assume_scheme="http")
 
     try:
         soup = http.get_soup('http://isup.me/' + domain)
-    except http.HTTPError as xxx_todo_changeme:
-        http.URLError = xxx_todo_changeme
-        return "Could not get status."
+    except http.HTTPError:
+        return "Failed to get status."
 
     content = soup.find('div').text.strip()
 

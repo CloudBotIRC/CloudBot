5610b34a2e107510729fcd41c46979f3c3042d3f
Dabo Ross <daboross@daboross.net>
2014-04-13 18:03:20 -0700

    Fix plpaste refresh support

--- plugins/plpaste.py
+++ plugins/plpaste.py
@@ -1,3 +1,4 @@
+import os
 from util import hook, web
 from os import listdir
 
@@ -5,7 +6,7 @@ from os import listdir
 @hook.command(permissions=["adminonly"])
 def plpaste(inp, bot=None):
     if inp in bot.commands:
-        with open(bot.commands[inp][0].func_code.co_filename.strip()) as f:
+        with open(os.path.join("plugins", bot.commands[inp][0].__code__.co_filename.strip())) as f:
             return web.haste(f.read(), ext='py')
     elif inp + ".py" in listdir('plugins/'):
         with open('plugins/{}.py'.format(inp)) as f:

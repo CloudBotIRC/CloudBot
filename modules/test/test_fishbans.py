6e62138ec1a9a6438842e6f84e44491679b931d4
Luke Rogers <lukeroge@gmail.com>
2014-04-22 16:33:45 +1200

    increase test coverage to 85%

--- modules/test/test_fishbans.py
+++ modules/test/test_fishbans.py
@@ -12,9 +12,15 @@ test_api = """
 test_api_no_bans = """
 {"success":true,"stats":{"username":"notch","uuid":"069a79f444e94726a5befca90e38aaf5","totalbans":0,"service":{"mcbans":0,"mcbouncer":0,"mcblockit":0,"minebans":0,"glizer":0}}}
 """
+test_api_failed = """
+{"success":false}
+"""
 
 bans_reply = "The user \x02notch\x02 has \x0211\x02 ban(s) - http://fishbans.com/u/notch/"
 count_reply = "Bans for \x02notch\x02: mcbouncer: \x0211\x02 - http://fishbans.com/u/notch/"
+
+failed_bans_reply = "Could not fetch ban data for notch."
+failed_count_reply = "Could not fetch ban data for notch."
 
 no_bans_reply = "The user \x02notch\x02 has no bans - http://fishbans.com/u/notch/"
 no_count_reply = "The user \x02notch\x02 has no bans - http://fishbans.com/u/notch/"
@@ -28,6 +34,16 @@ def test_bans(monkeypatch):
     monkeypatch.setattr(http, "get_json", fake_http)
 
     assert fishbans(test_user) == bans_reply
+
+
+def test_bans_failed(monkeypatch):
+    def fake_http(url):
+        assert url == "http://api.fishbans.com/stats/notch/"
+        return json.loads(test_api_failed)
+
+    monkeypatch.setattr(http, "get_json", fake_http)
+
+    assert fishbans(test_user) == failed_bans_reply
 
 
 def test_bans_no_bans(monkeypatch):
@@ -50,6 +66,16 @@ def test_count(monkeypatch):
     assert bancount(test_user) == count_reply
 
 
+def test_count_failed(monkeypatch):
+    def fake_http(url):
+        assert url == "http://api.fishbans.com/stats/notch/"
+        return json.loads(test_api_failed)
+
+    monkeypatch.setattr(http, "get_json", fake_http)
+
+    assert bancount(test_user) == failed_count_reply
+
+
 def test_count_no_bans(monkeypatch):
     def fake_http(url):
         assert url == "http://api.fishbans.com/stats/notch/"
@@ -57,4 +83,4 @@ def test_count_no_bans(monkeypatch):
 
     monkeypatch.setattr(http, "get_json", fake_http)
 
-    assert fishbans(test_user) == no_count_reply
+    assert bancount(test_user) == no_count_reply

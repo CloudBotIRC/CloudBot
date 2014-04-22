c27be1e41e617d47d6a88dc15af1e08471385519
Dabo Ross <daboross@daboross.net>
2014-04-21 22:12:16 -0700

    Re-assign text variable instead of assigning to unused inp variable in slogan plugin

--- modules/slogan.py
+++ modules/slogan.py
@@ -2,7 +2,6 @@ import random
 
 from util import hook, formatting
 
-
 with open("./data/slogans.txt") as f:
     slogans = [line.strip() for line in f.readlines()
                if not line.startswith("//")]
@@ -13,6 +12,6 @@ def slogan(text):
     """slogan <word> -- Makes a slogan for <word>."""
     out = random.choice(slogans)
     if text.lower() and out.startswith("<text>"):
-        inp = formatting.capitalize_first(text)
+        text = formatting.capitalize_first(text)
 
     return out.replace('<text>', text)

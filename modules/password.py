26e892cb1e5ca96e5968ecd8503d320078b0f3fa
Dabo Ross <daboross@daboross.net>
2014-04-21 23:02:51 -0700

    Add readable password command to password plugin. Also make the password plugin use the more secure pycrypto implementation of random if possible.

--- modules/password.py
+++ modules/password.py
@@ -1,19 +1,25 @@
-# TODO: Add some kind of pronounceable password generation
-# TODO: Improve randomness
 import string
-import random
+
+try:
+    from Crypto.Random import random
+except ImportError:
+    # Just use the regular random module, not the strong one
+    import random
 
 from util import hook
 
+with open("data/password_words.txt") as f:
+    common_words = [line.strip() for line in f.readlines()]
+
 
 @hook.command
-def password(inp, notice=None):
+def password(text, notice):
     """password <length> [types] -- Generates a password of <length> (default 10).
     [types] can include 'alpha', 'no caps', 'numeric', 'symbols' or any combination of the inp, eg. 'numbers symbols'"""
     okay = []
 
     # find the length needed for the password
-    numb = inp.split(" ")
+    numb = text.split(" ")
 
     try:
         length = int(numb[0])
@@ -21,18 +27,18 @@ def password(inp, notice=None):
         length = 10
 
     # add alpha characters
-    if "alpha" in inp or "letter" in inp:
+    if "alpha" in text or "letter" in text:
         okay = okay + list(string.ascii_lowercase)
         #adds capital characters if not told not to
-        if "no caps" not in inp:
+        if "no caps" not in text:
             okay = okay + list(string.ascii_uppercase)
 
     # add numbers
-    if "numeric" in inp or "number" in inp:
+    if "numeric" in text or "number" in text:
         okay = okay + [str(x) for x in range(0, 10)]
 
     # add symbols
-    if "symbol" in inp:
+    if "symbol" in text:
         sym = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '[', ']', '{', '}', '\\', '|', ';',
                ':', "'", '.', '>', ',', '<', '/', '?', '`', '~', '"']
         okay += okay + sym
@@ -48,3 +54,22 @@ def password(inp, notice=None):
         pw = pw + random.choice(okay)
 
     notice(pw)
+
+
+@hook.command(["rpass", "rpassword", "readablepassword"])
+def readable_password(text, notice):
+    """rpass [length]  -- Generates an easy to remember password with [length] (default 4) commonly used words."""
+    if text:
+        try:
+            length = int(text)
+        except ValueError:
+            notice("Invalid input '{}'".format(text))
+            return
+    else:
+        length = 4
+    words = []
+    # generate password
+    for x in range(length):
+        words.append(random.choice(common_words))
+
+    notice("Your password is '{}'. Feel free to remove the spaces when using it.".format(" ".join(words)))

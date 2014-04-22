ca3861bf29fb5ab12c455003a313a1b369a4eaa6
Dabo Ross <daboross@daboross.net>
2014-04-21 21:00:12 -0700

    Make kernel use new format

--- modules/kernel.py
+++ modules/kernel.py
@@ -4,12 +4,11 @@ from util import hook, http
 
 
 @hook.command(autohelp=False)
-def kernel(inp, reply=None):
+def kernel(reply):
     contents = http.get("https://www.kernel.org/finger_banner")
     contents = re.sub(r'The latest(\s*)', '', contents)
     contents = re.sub(r'version of the Linux kernel is:(\s*)', '- ', contents)
     lines = contents.split("\n")
 
-    message = "Linux kernel versions: "
-    message += ", ".join(line for line in lines[:-1])
+    message = "Linux kernel versions: {}".format(", ".join(line for line in lines[:-1]))
     reply(message)

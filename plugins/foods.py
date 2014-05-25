8d6eaeb8a9e8a861cfad8020bee8b38048e45fd9
Luke Rogers <lukeroge@gmail.com>
2014-05-25 15:43:12 +1200

    Combined all food-related plugins into foods.py

--- plugins/foods.py
+++ plugins/foods.py
@@ -1,8 +1,14 @@
-# coding=utf-8
 import re
 import random
 
 from cloudbot import hook
+
+cakes = ['Chocolate', 'Ice Cream', 'Angel', 'Boston Cream', 'Birthday', 'Bundt', 'Carrot', 'Coffee', 'Devils', 'Fruit',
+         'Gingerbread', 'Pound', 'Red Velvet', 'Stack', 'Welsh', 'Yokan']
+
+cookies = ['Chocolate Chip', 'Oatmeal', 'Sugar', 'Oatmeal Raisin', 'Macadamia Nut', 'Jam Thumbprint', 'Medican Wedding',
+           'Biscotti', 'Oatmeal Cranberry', 'Chocolate Fudge', 'Peanut Butter', 'Pumpkin', 'Lemon Bar',
+           'Chocolate Oatmeal Fudge', 'Toffee Peanut', 'Danish Sugar', 'Triple Chocolate', 'Oreo']
 
 potatoes = ['AC Belmont', 'AC Blue Pride', 'AC Brador', 'AC Chaleur', 'AC Domino', 'AC Dubuc', 'AC Glacier Chip',
             'AC Maple Gold', 'AC Novachip', 'AC Peregrine Red', 'AC Ptarmigan', 'AC Red Island', 'AC Saguenor',
@@ -30,7 +36,7 @@ potatoes = ['AC Belmont', 'AC Blue Pride', 'AC Brador', 'AC Chaleur', 'AC Domino
             'Purple Majesty', 'Purple Viking', 'Ranger Russet', 'Reba', 'Red Cloud', 'Red Gold', 'Red La Soda',
             'Red Pontiac', 'Red Ruby', 'Red Thumb', 'Redsen', 'Rocket', 'Rose Finn Apple', 'Rose Gold', 'Roselys',
             'Rote Erstling', 'Ruby Crescent', 'Russet Burbank', 'Russet Legend', 'Russet Norkotah', 'Russet Nugget',
-            'Russian Banana', 'Saginaw Gold', 'Sangre', 'Sant�', 'Satina', 'Saxon', 'Sebago', 'Shepody', 'Sierra',
+            'Russian Banana', 'Saginaw Gold', 'Sangre', 'Satina', 'Saxon', 'Sebago', 'Shepody', 'Sierra',
             'Silverton Russet', 'Simcoe', 'Snowden', 'Spunta', "St. John's", 'Summit Russet', 'Sunrise', 'Superior',
             'Symfonia', 'Tolaas', 'Trent', 'True Blue', 'Ulla', 'Umatilla Russet', 'Valisa', 'Van Gogh', 'Viking',
             'Wallowa Russet', 'Warba', 'Western Russet', 'White Rose', 'Willamette', 'Winema', 'Yellow Finn',
@@ -38,11 +44,11 @@ potatoes = ['AC Belmont', 'AC Blue Pride', 'AC Brador', 'AC Chaleur', 'AC Domino
 
 
 @hook.command
-def potato(inp, action=None):
+def potato(text, action=None):
     """potato <user> - Makes <user> a tasty little potato."""
-    inp = inp.strip()
+    text = text.strip()
 
-    if not re.match("^[A-Za-z0-9_|.-\]\[]*$", inp.lower()):
+    if not re.match("^[A-Za-z0-9_|.-\]\[]*$", text.lower()):
         return "I cant make a tasty potato for that user!"
 
     potato_type = random.choice(potatoes)
@@ -51,5 +57,41 @@ def potato(inp, action=None):
     method = random.choice(['bakes', 'fries', 'boils', 'roasts'])
     side_dish = random.choice(['side salad', 'dollop of sour cream', 'piece of chicken', 'bowl of shredded bacon'])
 
-    action("{} a {} {} {} potato for {} and serves it with a small {}!".format(method, flavor, size, potato_type, inp,
+    action("{} a {} {} {} potato for {} and serves it with a small {}!".format(method, flavor, size, potato_type, text,
                                                                                side_dish))
+
+@hook.command
+def cake(text, action):
+    """cake <user> - Gives <user> an awesome cake."""
+    text = text.strip()
+
+    if not re.match("^[A-Za-z0-9_|.-\]\[]*$", text.lower()):
+        return "I can't give an awesome cake to that user!"
+
+    cake_type = random.choice(cakes)
+    size = random.choice(['small', 'little', 'mid-sized', 'medium-sized', 'large', 'gigantic'])
+    flavor = random.choice(['tasty', 'delectable', 'delicious', 'yummy', 'toothsome', 'scrumptious', 'luscious'])
+    method = random.choice(['makes', 'gives', 'gets', 'buys'])
+    side_dish = random.choice(['glass of chocolate milk', 'bowl of ice cream', 'jar of cookies',
+                               'some chocolate sauce'])
+
+    action("{} {} a {} {} {} cake and serves it with a small {}!".format(method, text, flavor, size, cake_type,
+                                                                         side_dish))
+
+
+@hook.command
+def cookie(text, action):
+    """cookie <user> - Gives <user> a cookie"""
+    text = text.strip()
+
+    if not re.match("^[A-Za-z0-9_|.-\]\[]*$", text.lower()):
+        return "I can't give a cookie to that user!"
+
+    cookie_type = random.choice(cookies)
+    size = random.choice(['small', 'little', 'medium-sized', 'large', 'gigantic'])
+    flavor = random.choice(['tasty', 'delectable', 'delicious', 'yummy', 'toothsome', 'scrumptious', 'luscious'])
+    method = random.choice(['makes', 'gives', 'gets', 'buys'])
+    side_dish = random.choice(['glass of milk', 'bowl of ice cream', 'bowl of chocolate sauce'])
+
+    action("{} {} a {} {} {} cookie and serves it with a {}!".format(method, text, flavor, size, cookie_type,
+                                                                     side_dish))

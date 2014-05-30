5ff3f160940f4f2ee0746c50060deddd214b1773
Zarthus <zarthus@zarth.us>
2014-05-30 18:59:54 +0200

    wow.py: Finishing touches, improved regex, clearer error messages, few bugfixes.

--- plugins/wow.py
+++ plugins/wow.py
@@ -5,8 +5,6 @@ Gets data from the World of Warcraft Armoury API
 
 Commands:
 armoury, armory: Request data from the armoury API and format it into something human readable.
-
-Revision 0
 """
 
 import re 
@@ -23,30 +21,32 @@ def armoury(inp):
 
     if len(splitinput) < 2:
         return 'armoury [realm] [character name] [region = EU] - Look up character and returns API data.'
-        
-    realm = splitinput[0]
-    charname = splitinput[1]
     
+    realm = splitinput[0].replace('_', '-')
+    charname = splitinput[1]
+
+    # Sets the default region to EU if none specified.
     if len(splitinput) < 3:
         region = 'eu'
     else:
         region = splitinput[2]
-        
-    if not re.match("^[a-z]{1,3}$", region):
+            
+    if not re.match(r"^[a-z]{1,3}$", region):
         return 'The region specified is not a valid region. Valid regions: eu, us, sea, kr, tw.'
 
-    if not re.match("^[\d!@#$%^&*()_+]$", charname) or len(charname) > 18: 
-        # May not contain digits, or repeat the same letter three times, or contain !@#%^&()_+[]. 
-        # Special characters are permitted, such as áéíóßø.nano
-        return 'The character name is not a valid name. Character names can only contain letters, and be 18 characters long.'
+    if re.match(r"^[^\d]$", charname) or len(charname) > 18: 
+        # May not contain digits, repeat the same letter three times, or contain non-word characters. 
+        # Special characters are permitted, such as áéíóßø. 
+        return 'The character name is not a valid name. Character names can only contain letters, special characters, and be 18 characters long.'
     
-    if not re.match("^[a-z]{3,32}$", realm):
-        return 'The realm name is not a valid name. Realm names can only contain letters.'
+    if not re.match(r"^[a-z' _-]{3,32}$", realm):
+		# Realm names can have spaces in them, use dashes for this.
+        return 'The realm name is not a valid name. Realm names can only contain letters, dashes, and apostrophes, up to 32 characters'
     
     region_short = wow_region_shortname(region)
 
     if not region_short:
-        return 'The region "{}" is not supported.'.format(region)
+        return 'The region \'{}\' does not exist.'.format(region)
 
     link = "http://{0}.battle.net/api/wow/character/{1}/{2}".format(region, realm, charname)
     
@@ -57,7 +57,7 @@ def wow_armoury_data(link):
     try:    
         data = http.get_json(link)
     except Exception as e:
-        return 'Could not fetch data for {}, error: {}'.format(link, str(e))
+        return 'Unable to fetch information for {}. Does the realm or character exist? ({})'.format(link, str(e))
         
     return wow_armoury_format(data, link)
 
@@ -72,14 +72,14 @@ def wow_armoury_format(data, link):
         return data['reason']
         
     if 'name' in data:
+        niceurl = link.replace('/api/wow/', '/wow/en/') + '/simple'
+
         try:
-            niceurl = link.replace('/api/wow/', '/wow/en/') + '/simple'
-            
             return '{0} is a level \x0307{1}\x0F {2} {3} on {4} with \x0307{5}\x0F achievement points and \x0307{6}\x0F honourable kills. Armoury Profile: {7}' \
                 .format(data['name'], data['level'], wow_get_gender(data['gender']), wow_get_class(data['class'], True), data['realm'], 
-                data['achievementPoints'], data['totalHonorableKills'], web.isgd(niceurl))
+                    data['achievementPoints'], data['totalHonorableKills'], web.isgd(niceurl))
         except Exception as e:
-            return 'An error occured within the API request for {}, error: {}'.format(link, str(e))
+			return 'Unable to fetch information for {}. Does the realm or character exist? ({})'.format(niceurl, str(e))
             
     return 'An unexpected error occured.'
     
@@ -97,11 +97,18 @@ def wow_get_gender(genderid):
 def wow_get_class(classid, colours = False):
     """Formats a class ID to a readable name, data from http://eu.battle.net/api/wow/data/character/classes"""
     if colours:
-        classids = {1: "\x0305Warrior\x0F", 2: "\x0313Paladin\x0F", 3: "\x0303Hunter\x0F", 4: "\x0308Rogue\x0F", 5: "Priest", 6: "\x0304Death Knight\x0F", 
-            7: "\x0310Shaman\x0F", 8: "\x0311Mage\x0F", 9: "\x0306Warlock\x0F", 10: "\x0309Monk\x0F", 11: "\x0307Druid\x0F"}
+		# Format their colours according to class colours.
+        classids = {
+            1: "\x0305Warrior\x0F", 2: "\x0313Paladin\x0F", 3: "\x0303Hunter\x0F", 4: "\x0308Rogue\x0F", 
+            5: "Priest", 6: "\x0304Death Knight\x0F", 7: "\x0310Shaman\x0F", 8: "\x0311Mage\x0F", 
+            9: "\x0306Warlock\x0F", 10: "\x0309Monk\x0F", 11: "\x0307Druid\x0F"
+        }
     else:
-        classids = {1: "Warrior", 2: "Paladin", 3: "Hunter", 4: "Rogue", 5: "Priest", 6: "Death Knight", 7: "Shaman",
-            8: "Mage", 9: "Warlock", 10: "Monk", 11: "Druid"}
+        classids = {
+            1: "Warrior", 2: "Paladin", 3: "Hunter", 4: "Rogue", 5: "Priest", 
+            6: "Death Knight", 7: "Shaman", 8: "Mage", 9: "Warlock", 10: "Monk", 
+            11: "Druid"
+        }
         
             
     if classid in classids:
@@ -111,9 +118,11 @@ def wow_get_class(classid, colours = False):
         
 def wow_get_race(raceid):
     """Formats a race ID to a readable race name, data from http://eu.battle.net/api/wow/data/character/races"""
-    raceids = {1: "Human", 2: "Orc", 3: "Dwarf", 4: "Night Elf", 5: "Undead", 6: "Tauren", 7: "Gnome", 
-        8: "Troll", 9: "Goblin", 10: "Blood Elf", 11: "Draenei", 22: "Worgen", 24: "Pandaren (neutral)", 
-        25: "Pandaren (alliance)", 26: "Pandaren (horde)"}
+    raceids = {
+        1: "Human", 2: "Orc", 3: "Dwarf", 4: "Night Elf", 5: "Undead", 6: "Tauren", 7: "Gnome", 
+        8: "Troll", 9: "Goblin", 10: "Blood Elf", 11: "Draenei", 22: "Worgen", 
+        24: "Pandaren (neutral)", 25: "Pandaren (alliance)", 26: "Pandaren (horde)"
+    }
 
     if raceid in raceids:
         return raceids[raceid]
@@ -122,8 +131,13 @@ def wow_get_race(raceid):
     
 def wow_region_shortname(region):
     """Returns a short region name, which functions as battle.net their subdomain (i.e. eu.battle.net)"""
-    validregions = {'eu': 'eu', 'europe': 'eu', 'us': 'us', 'sea': 'sea', 'asia': 'sea', 'kr': 'kr', 'korea': 'kr',
-        'tw': 'tw', 'taiwan': 'tw'}
+    validregions = {
+        'eu': 'eu', 'europe': 'eu', 
+        'us': 'us', 
+        'sea': 'sea', 'asia': 'sea', 
+        'kr': 'kr', 'korea': 'kr',
+        'tw': 'tw', 'taiwan': 'tw'
+    }
 
     if region in validregions:
         return validregions[region]

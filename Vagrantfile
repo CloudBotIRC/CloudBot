b313bc062ae68889e0f857429a99389da7a0bdf0
Dabo Ross <daboross@daboross.net>
2014-06-03 03:07:05 -0700

    Add vagrant files

---
+++ Vagrantfile
@@ -0,0 +1,6 @@
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty32"
+  config.vm.provision :shell, path: "vagrant-bootstrap.sh"
+end
---
+++ vagrant-bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# update sources
+sudo apt-get update
+
+# install things:
+# pip               so we can install rependencies
+# git               so pip can clone from repository
+# libenchant-dev    so python-enchant can use it
+# python3-lxml      so we can use precompiled binaries instead of compiling them
+
+sudo apt-get install -y python3-pip git libenchant-dev python3-lxml
+
+# install requirements using pip
+sudo pip3 install -r /vagrant/requirements.txt
+
+# create link from project to ~/bot
+ln -sf /vagrant /home/vagrant/bot
+
+# create start.sh script
+cat > /home/vagrant/start.sh << EOF
+#!/usr/bin/env bash
+cd /home/vagrant/bot
+python3 -m cloudbot
+EOF

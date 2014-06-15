8e924cc93e440d79dccd31faa48a94e9a0458a50
Dabo Ross <daboross@daboross.net>
2014-06-15 15:22:39 -0700

    Add support for server-side JOIN command for the bot's nick to core_tracker, so that the bot knows what channels it is in when it connects to a BNC which saves channels.

--- plugins/core_tracker.py
+++ plugins/core_tracker.py
@@ -1,35 +1,37 @@
 # plugin to keep track of bot state
 
 import asyncio
+import logging
 import re
 
 from cloudbot import hook
+
+logger = logging.getLogger("cloudbot")
 
 nick_re = re.compile(":(.+?)!")
 
 
 @asyncio.coroutine
 @hook.irc_raw("KICK")
-def on_kick(irc_paramlist, conn, chan):
+def on_kick(conn, chan, nick):
     """
-    :type irc_paramlist: list[str]
     :type conn: cloudbot.core.connection.BotConnection
     :type chan: str
+    :type nick: str
     """
     # if the bot has been kicked, remove from the channel list
-    if irc_paramlist[1] == conn.nick:
-        conn.channels.remove(chan)
-        auto_rejoin = conn.config.get('auto_rejoin', False)
-        if auto_rejoin:
-            conn.join(irc_paramlist[0])
+    if nick == conn.nick:
+        if chan in conn.channels:
+            conn.channels.remove(chan)
+        if conn.config.get('auto_rejoin', False):
+            conn.join(chan)
 
 
 @asyncio.coroutine
 @hook.irc_raw("NICK")
-def on_nick(irc_paramlist, bot, conn, irc_raw):
+def on_nick(irc_paramlist, conn, irc_raw):
     """
     :type irc_paramlist: list[str]
-    :type bot: cloudbot.core.bot.CloudBot
     :type conn: cloudbot.core.connection.BotConnection
     :type irc_raw: str
     """
@@ -37,4 +39,19 @@ def on_nick(irc_paramlist, bot, conn, irc_raw):
     new_nick = str(irc_paramlist[0])
     if old_nick == conn.nick:
         conn.nick = new_nick
-        bot.logger.info("Bot nick changed from '{}' to '{}'.".format(old_nick, new_nick))
+        logger.info("Bot nick changed from '{}' to '{}'.".format(old_nick, new_nick))
+
+
+# for channels the host tells us we're joining without us joining it ourselves
+# mostly when using a BNC which saves channels
+@asyncio.coroutine
+@hook.irc_raw("JOIN")
+def on_join(conn, chan, nick):
+    """
+    :type conn: cloudbot.core.connection.BotConnection
+    :type chan: str
+    :type nick: str
+    """
+    if nick == conn.nick:
+        if chan not in conn.channels:
+            conn.channels.append(chan)

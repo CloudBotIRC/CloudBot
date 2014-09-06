fea70da9a58a147820814beb18b28ff0c1bb0d29
Daniel Barnes <nasonfish@nasonfish.com>
2014-09-06 16:42:34 -0600

    Add flag support for corrections.py

--- plugins/correction.py
+++ plugins/correction.py
@@ -3,7 +3,7 @@ import re
 
 from cloudbot import hook
 
-correction_re = re.compile(r"^[sS]/(.*/.*)/\S*$")
+correction_re = re.compile(r"^[sS]/(.*/.*/[igx]{,4})\S*$")
 
 
 @asyncio.coroutine
@@ -14,10 +14,9 @@ def correction(match, conn, chan, message):
     :type conn: cloudbot.client.Client
     :type chan: str
     """
-    print(match.groups())
-    find, replacement = tuple([b.replace("\/", "/") for b in re.split(r"(?<!\\)/", match.groups()[0])])
+    find, replacement, flags = tuple([b.replace("\/", "/") for b in re.split(r"(?<!\\)/", match.groups()[0])])
 
-    find_re = re.compile("(?i){}".format(find))
+    find_re = re.compile("{}{}".format("(?{})".format(flags.replace("g", "")) if flags.replace("g", "") != "" else "", find))
 
     for item in conn.history[chan].__reversed__():
         nick, timestamp, msg = item
@@ -27,8 +26,9 @@ def correction(match, conn, chan, message):
         if find_re.search(msg):
             if "\x01ACTION" in msg:
                 msg = msg.replace("\x01ACTION ", "/me ").replace("\x01", "")
-            message("Correction, <{}> {}".format(nick, find_re.sub("\x02" + replacement + "\x02", msg)))
+            message("Correction, <{}> {}".format(nick, find_re.sub("\x02" + replacement + "\x02", msg, count=int(not "g" in flags))))
             return
         else:
             continue
     return "Did not find {} in any recent messages.".format(to_find)
+

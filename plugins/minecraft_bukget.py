d98b459c520f1a172e8c9227ba209996eff0cd24
Luke <lukeroge@gmail.com>
2014-11-22 18:13:40 +1300

    we don't bold links dammit blha

--- plugins/minecraft_bukget.py
+++ plugins/minecraft_bukget.py
@@ -126,7 +126,7 @@ def format_output(data):
     else:
         line_a = "\x02{}\x02, by \x02{}\x02 ({}) \x02{}".format(name, authors, stage, url)
 
-    line_b = "Last release: \x02v{}\x02 for \x02{}\x02 at {} \x02{}\x02".format(version_number, bukkit_versions,
+    line_b = "Last release: \x02v{}\x02 for \x02{}\x02 at {} {}".format(version_number, bukkit_versions,
                                                                                 last_update, link)
 
     return line_a, line_b

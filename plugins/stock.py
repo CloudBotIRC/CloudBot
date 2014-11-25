1c6ec3b7ae6fe67b6288ff20632a060b2b119046
Luke <lukeroge@gmail.com>
2014-11-26 01:39:48 +1300

    Updated stock plugin to use requests

--- plugins/stock.py
+++ plugins/stock.py
@@ -1,14 +1,35 @@
+from urllib.parse import quote_plus
+
+import requests
+
 from cloudbot import hook
-from cloudbot.util import web
+
+BASE_URL = "http://query.yahooapis.com/v1/public/yql"
+ENV = "http://datatables.org/alltables.env"
+
+
+def get_data(symbol):
+    query = 'SELECT * FROM yahoo.finance.quote WHERE symbol="{}" LIMIT 1'.format(quote_plus(symbol))
+    request = requests.get(BASE_URL, params={'q': query, 'env': ENV, 'format': 'json'})
+    request.raise_for_status()
+
+    return request.json()['query']
 
 
 @hook.command()
 def stock(text):
-    """stock <symbol> -- gets stock information"""
+    """<symbol> -- gets stock information"""
     sym = text.strip().lower()
 
-    query = "SELECT * FROM yahoo.finance.quote WHERE symbol=@symbol LIMIT 1"
-    quote = web.query(query, {"symbol": sym}).one()
+    try:
+        data = get_data(text)
+    except requests.exceptions.HTTPError as e:
+        return "Could not get stock data: {}".format(e)
+
+    if not data["results"]:
+        return "No results."
+
+    quote = data['results']['quote']
 
     # if we don't get a company name back, the symbol doesn't match a company
     if quote['Change'] is None:
@@ -17,15 +38,18 @@ def stock(text):
     change = float(quote['Change'])
     price = float(quote['LastTradePriceOnly'])
 
+    # this is for dead companies, if this isn't here PercentChange will fail with DBZ
+    if price == 0 and change == 0:
+        return "\x02{Name}\x02 (\x02{symbol}\x02) - {LastTradePriceOnly}".format(**quote)
+
     if change < 0:
         quote['color'] = "5"
     else:
         quote['color'] = "3"
 
     quote['PercentChange'] = 100 * change / (price - change)
-    print(quote)
 
-    return "\x02{Name}\x02 (\x02{symbol}\x02) - {LastTradePriceOnly} " \
+    return "\x02{Name}\x02 (\x02{symbol}\x02): {LastTradePriceOnly} " \
            "\x03{color}{Change} ({PercentChange:.2f}%)\x03 " \
-           "Day Range: {DaysRange} " \
+           "- Day Range: {DaysRange} " \
            "MCAP: {MarketCapitalization}".format(**quote)

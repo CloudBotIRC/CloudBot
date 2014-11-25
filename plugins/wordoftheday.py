b45d4756e1934ae8dc3a2efa59b13bc2a6308f97
Luke <lukeroge@gmail.com>
2014-11-26 01:02:44 +1300

    Word of the day fixed up and requestified!

--- plugins/wordoftheday.py
+++ plugins/wordoftheday.py
@@ -1,16 +1,18 @@
-from cloudbot import hook
-from cloudbot.util import http
+import requests
 from bs4 import BeautifulSoup
 
+from cloudbot import hook
 
-@hook.command(autohelp=False)
-def word():
-    """<word> - Gets the word of the day from http://www.merriam-webster.com/word-of-theday"""
-    page = http.get('http://merriam-webster.com/word-of-the-day')
 
-    soup = BeautifulSoup(page)
+@hook.command("wordoftheday", "word", autohelp=False)
+def wordoftheday():
+    """- Gets the word of the day from http://www.merriam-webster.com/word-of-theday"""
+    page = requests.get('http://merriam-webster.com/word-of-the-day')
 
-    word = soup.find('strong', {'class': 'main_entry_word'}).string
-    function = soup.find('p', {'class': 'word_function'}).string
+    soup = BeautifulSoup(page.text)
 
-    return "The word of the day is: \x02{}\x02 ({})".format(word, function)
+    word = soup.find('strong', {'class': 'main_entry_word'}).text
+    function = soup.find('p', {'class': 'word_function'}).text
+    definition = soup.find('div', {'class': 'scnt'}).find('span', {'class': 'ssens'}).text
+
+    return "The word of the day is: \x02{}\x02 ({}){}".format(word, function, definition)

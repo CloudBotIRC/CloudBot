fab8a1f2586292511ba0d3b141b243c946349e17
Luke <lukeroge@gmail.com>
2014-12-11 23:05:29 +1300

    lmgtfy uses http.py no more

--- plugins/lmgtfy.py
+++ plugins/lmgtfy.py
@@ -1,11 +1,13 @@
+import requests
+
 from cloudbot import hook
-from cloudbot.util import web, http
+from cloudbot.util import web
 
 
 @hook.command("lmgtfy", "gfy")
 def lmgtfy(text):
     """[phrase] - gets a lmgtfy.com link for the specified phrase"""
 
-    link = "http://lmgtfy.com/?q={}".format(http.quote_plus(text))
+    link = "http://lmgtfy.com/?q={}".format(requests.utils.quote(text))
 
     return web.try_shorten(link)

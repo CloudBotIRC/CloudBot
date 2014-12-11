02d525a1e5566850819536e1ec8663327f41a260
Luke <lukeroge@gmail.com>
2014-12-12 03:42:20 +1300

    snopes ported to requests

--- plugins/snopes.py
+++ plugins/snopes.py
@@ -1,22 +1,39 @@
 import re
 
+import requests
+from lxml import html
+
 from cloudbot import hook
-from cloudbot.util import http
+from cloudbot.util import formatting, web
+
 
 search_url = "http://search.atomz.com/search/?sp_a=00062d45-sp00000000"
 
 
-@hook.command()
+@hook.command
 def snopes(text):
     """snopes <topic> -- Searches snopes for an urban legend about <topic>."""
 
-    search_page = http.get_html(search_url, sp_q=text, sp_c="1")
+    try:
+        params = {'sp_q': text, 'sp_c': "1"}
+        request = requests.get(search_url, params=params)
+        request.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+        return "Error finding results: {}".format(e)
+
+    search_page = html.fromstring(request.text)
     result_urls = search_page.xpath("//a[@target='_self']/@href")
 
     if not result_urls:
-        return "no matching pages found"
+        return "No matching pages found."
 
-    snopes_page = http.get_html(result_urls[0])
+    try:
+        _request = requests.get(result_urls[0])
+        _request.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+        return "Error finding results: {}".format(e)
+
+    snopes_page = html.fromstring(_request.text)
     snopes_text = snopes_page.text_content()
 
     claim = re.search(r"Claim: .*", snopes_text).group(0).strip()
@@ -25,10 +42,12 @@ def snopes(text):
     if status is not None:
         status = status.group(0).strip()
     else:  # new-style statuses
-        status = "Status: {}.".format(re.search(r"FALSE|TRUE|MIXTURE|UNDETERMINED",
-                                                snopes_text).group(0).title())
+        status = "Status: {}".format(re.search(r"FALSE|TRUE|MIXTURE|UNDETERMINED",
+                                               snopes_text).group(0).title())
 
-    claim = re.sub(r"[\s\xa0]+", " ", claim)  # compress whitespace
-    status = re.sub(r"[\s\xa0]+", " ", status)
+    status = " ".join(status.split())  # compress whitespace
+    claim = formatting.truncate_str(" ".join(claim.split()), 150)
 
-    return "{} {} {}".format(claim, status, result_urls[0])
+    url = web.try_shorten(result_urls[0])
+
+    return '"{}" {} - {}'.format(claim, status, url)

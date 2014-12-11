a88d37f0f5f487fa248b26d9295d4c2923872ee8
Luke <lukeroge@gmail.com>
2014-12-11 23:59:59 +1300

    port mlia to requests using fmylife code

--- plugins/mlia.py
+++ plugins/mlia.py
@@ -1,34 +1,44 @@
-# Plugin by Infinity - <https://github.com/infinitylabs/UguuBot>
-
+import asyncio
 import random
 
+from bs4 import BeautifulSoup
+import requests
+
 from cloudbot import hook
-from cloudbot.util import http
 
 mlia_cache = []
 
 
-def refresh_cache():
-    """gets a page of random MLIAs and puts them into a dictionary """
+@asyncio.coroutine
+def refresh_cache(loop):
+    """ gets a page of random MLIAs and puts them into a dictionary """
     url = 'http://mylifeisaverage.com/{}'.format(random.randint(1, 11000))
-    soup = http.get_soup(url)
+    request = yield from loop.run_in_executor(None, requests.get, url)
+    soup = BeautifulSoup(request.text, "lxml")
 
     for story in soup.find_all('div', {'class': 'story '}):
         mlia_id = story.find('span', {'class': 'left'}).a.text
-        mlia_text = story.find('div', {'class': 'sc'}).text.strip()
+        mlia_text = story.find('div', {'class': 'sc'}).text
+        mlia_text = " ".join(mlia_text.split())
         mlia_cache.append((mlia_id, mlia_text))
 
-# do an initial refresh of the cache
-refresh_cache()
+
+@asyncio.coroutine
+@hook.onload()
+def initial_refresh(loop):
+    # do an initial refresh of the cache
+    yield from refresh_cache(loop)
 
 
+@asyncio.coroutine
 @hook.command(autohelp=False)
-def mlia(reply):
+def mlia(reply, loop):
     """- gets a random quote from MyLifeIsAverage.com"""
+
     # grab the last item in the mlia cache and remove it
     mlia_id, text = mlia_cache.pop()
     # reply with the mlia we grabbed
     reply('({}) {}'.format(mlia_id, text))
     # refresh mlia cache if its getting empty
     if len(mlia_cache) < 3:
-        refresh_cache()
+        yield from refresh_cache(loop)

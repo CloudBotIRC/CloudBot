5bcc81cf2e396da34cf1b6b552e91462c7decb38
Andrew Edwards <andrewthomasedwards@gmail.com>
2014-12-20 08:44:39 -0500

    pep8

--- plugins/karma.py
+++ plugins/karma.py
@@ -7,13 +7,15 @@ karmaplus_re = re.compile('^.*\+\+$')
 karmaminus_re = re.compile('^.*\-\-$')
 db_ready = []
 
+
 def db_init(db, conn_name):
     """Check to see if the DB has the herald table. Connection name is for caching the result per connection.
     :type db: sqlalchemy.orm.Session
     """
     global db_ready
     if db_ready.count(conn_name) < 1:
-        db.execute("create table if not exists karma(name, chan, thing, score INTEGER, primary key(name, chan, thing))")
+        db.execute(
+            "create table if not exists karma(name, chan, thing, score INTEGER, primary key(name, chan, thing))")
         db.commit()
         db_ready.append(conn_name)
 
@@ -21,19 +23,23 @@ def db_init(db, conn_name):
 @hook.command("pp", "addpoint")
 def addpoint(text, nick, chan, db, conn):
     """.addpoint or (.pp) <thing> adds a point to the <thing>"""
-    
+
     db_init(db, conn.name)
-    karma = db.execute("select score from karma where name = :name and chan = :chan and thing = :thing", {'name':nick, 'chan': chan, 'thing': text.lower()}).fetchone()
+    karma = db.execute("select score from karma where name = :name and chan = :chan and thing = :thing", {
+                       'name': nick, 'chan': chan, 'thing': text.lower()}).fetchone()
     if karma:
         score = int(karma[0])
         score = score + 1
-        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {'name': nick, 'chan': chan, 'thing': text.lower(), 'score': score})
+        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {
+                   'name': nick, 'chan': chan, 'thing': text.lower(), 'score': score})
         db.commit()
         # return "{} is now worth {} in {}'s eyes.".format(text, score, nick)
     else:
-        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {'name': nick, 'chan': chan, 'thing': text.lower(), 'score': 1})
+        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {
+                   'name': nick, 'chan': chan, 'thing': text.lower(), 'score': 1})
         db.commit()
-        #return "{} is now worth 1 in {}'s eyes.".format(text, nick)
+        # return "{} is now worth 1 in {}'s eyes.".format(text, nick)
+
 
 @hook.regex(karmaplus_re)
 def re_addpt(match, nick, chan, db, conn, notice):
@@ -41,26 +47,30 @@ def re_addpt(match, nick, chan, db, conn, notice):
     thing = match.group().split('++')[0]
     if thing:
         addpoint(thing, nick, chan, db, conn)
-        #return out
+        # return out
     else:
         notice(pluspts(nick, chan, db, conn))
+
 
 @hook.command("mm", "rmpoint")
 def rmpoint(text, nick, chan, db, conn):
     """.rmpoint or (.mm) <thing> subtracts a point from the <thing>"""
 
     db_init(db, conn.name)
-    karma = db.execute("select score from karma where name = :name and chan = :chan and thing = :thing", {'name':nick, 'chan': chan, 'thing': text.lower()}).fetchone()
+    karma = db.execute("select score from karma where name = :name and chan = :chan and thing = :thing", {
+                       'name': nick, 'chan': chan, 'thing': text.lower()}).fetchone()
     if karma:
         score = int(karma[0])
         score = score - 1
-        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {'name': nick, 'chan': chan, 'thing': text.lower(), 'score': score})
+        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {
+                   'name': nick, 'chan': chan, 'thing': text.lower(), 'score': score})
         db.commit()
-        #return "{} is now worth {} in {}'s eyes.".format(text, score, nick)
+        # return "{} is now worth {} in {}'s eyes.".format(text, score, nick)
     else:
-        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {'name': nick, 'chan': chan, 'thing': text.lower(), 'score': -1})
+        db.execute("insert or replace into karma(name, chan, thing, score) values (:name, :chan, :thing, :score)", {
+                   'name': nick, 'chan': chan, 'thing': text.lower(), 'score': -1})
         db.commit()
-        #return "{} is now worth -1 in {}'s eyes.".format(text, nick)
+        # return "{} is now worth -1 in {}'s eyes.".format(text, nick)
 
 
 @hook.command("pluspts", autohelp=False)
@@ -68,20 +78,24 @@ def pluspts(nick, chan, db, conn):
     """prints the things you have liked"""
     db_init(db, conn.name)
     output = ""
-    likes = db.execute("select thing, score from karma where name = :name and chan = :chan and score >= 0 order by score desc", { 'name': nick, 'chan': chan }).fetchall()
+    likes = db.execute("select thing, score from karma where name = :name and chan = :chan and score >= 0 order by score desc", {
+                       'name': nick, 'chan': chan}).fetchall()
     for like in likes:
-       output = output + str(like[0]) + " has " + str(like[1]) + " points "
+        output = output + str(like[0]) + " has " + str(like[1]) + " points "
     return output
+
 
 @hook.command("minuspts", autohelp=False)
 def minuspts(nick, chan, db, conn):
     """prints the things you have liked"""
     db_init(db, conn.name)
     output = ""
-    likes = db.execute("select thing, score from karma where name = :name and chan = :chan and score <= 0 order by score", { 'name': nick, 'chan': chan }).fetchall()
+    likes = db.execute("select thing, score from karma where name = :name and chan = :chan and score <= 0 order by score", {
+                       'name': nick, 'chan': chan}).fetchall()
     for like in likes:
-       output = output + str(like[0]) + " has " + str(like[1]) + " points "
+        output = output + str(like[0]) + " has " + str(like[1]) + " points "
     return output
+
 
 @hook.regex(karmaminus_re)
 def re_rmpt(match, nick, chan, db, conn, notice):
@@ -89,7 +103,7 @@ def re_rmpt(match, nick, chan, db, conn, notice):
     thing = match.group().split('--')[0]
     if thing:
         rmpoint(thing, nick, chan, db, conn)
-        #return out
+        # return out
     else:
         notice(minuspts(nick, chan, db, conn))
 
@@ -99,7 +113,8 @@ def points(text, chan, db, conn):
     """.points <thing> will print the total points for <thing> in the channel."""
     db_init(db, conn.name)
     score = 0
-    karma = db.execute("select score from karma where thing = :thing and chan = :chan", {'thing': text.lower(), 'chan': chan }).fetchall()
+    karma = db.execute("select score from karma where thing = :thing and chan = :chan", {
+                       'thing': text.lower(), 'chan': chan}).fetchall()
     if karma:
         for k in karma:
             score = score + int(k[0])
--- plugins/sportscores.py
+++ plugins/sportscores.py
@@ -4,112 +4,122 @@ from cloudbot.util import http
 
 """This plugin is really messy I promise I will clean it up later."""
 
+
 @hook.command("nfl", autohelp=False)
 def nflScores(text=" "):
     """nfl <team> gets the score or next schedule game for the specified team. If no team specified all games will be included."""
-    response = http.get_html('http://scores.espn.go.com/nfl/bottomline/scores', decode=False)
+    response = http.get_html(
+        'http://scores.espn.go.com/nfl/bottomline/scores', decode=False)
     game = ""
     score = response.text_content()
-    raw=score.replace('%20',' ')
-    raw=raw.replace('^','')
-    raw=raw.replace('&','\n')
+    raw = score.replace('%20', ' ')
+    raw = raw.replace('^', '')
+    raw = raw.replace('&', '\n')
     pattern = re.compile("nfl_s_left\d+=(.*)")
     for match in re.findall(pattern, raw):
         if text.lower() in match.lower():
-            game = game +  match + "  "
+            game = game + match + "  "
     return(game)
 
 
 @hook.command("mlb", autohelp=False)
 def mlbScores(text=" "):
     """mlb <team city> gets the score or next scheduled game for the specified team. If no team is specified all games will be included."""
-    response = http.get_html('http://scores.espn.go.com/mlb/bottomline/scores', decode=False)
+    response = http.get_html(
+        'http://scores.espn.go.com/mlb/bottomline/scores', decode=False)
     game = ""
     score = response.text_content()
-    raw=score.replace('%20',' ')
-    raw=raw.replace('^','')
-    raw=raw.replace('&','\n')
+    raw = score.replace('%20', ' ')
+    raw = raw.replace('^', '')
+    raw = raw.replace('&', '\n')
     pattern = re.compile("mlb_s_left\d+=(.*)")
     for match in re.findall(pattern, raw):
         if text.lower() in match.lower():
-            game = game +  match + "  "
+            game = game + match + "  "
     return(game)
 
 
 @hook.command("nba", autohelp=False)
 def nbaScores(text=" "):
     """nba <team city> gets the score or next scheduled game for the specified team. If no team is specified all games will be included."""
-    response = http.get_html('http://scores.espn.go.com/nba/bottomline/scores', decode=False)
+    response = http.get_html(
+        'http://scores.espn.go.com/nba/bottomline/scores', decode=False)
     game = ""
     score = response.text_content()
-    raw=score.replace('%20',' ')
-    raw=raw.replace('^','')
-    raw=raw.replace('&','\n')
+    raw = score.replace('%20', ' ')
+    raw = raw.replace('^', '')
+    raw = raw.replace('&', '\n')
     pattern = re.compile("nba_s_left\d+=(.*)")
     for match in re.findall(pattern, raw):
         if text.lower() in match.lower():
-            game = game +  match + "  "
+            game = game + match + "  "
     return(game)
 
 
 @hook.command("ncaab", autohelp=False)
 def ncaabScores(text=" "):
     """ncaab <team city> gets the score or next scheduled game for the specified team. If no team is specified all games will be included."""
-    response = http.get_html('http://scores.espn.go.com/ncb/bottomline/scores', decode=False)
+    response = http.get_html(
+        'http://scores.espn.go.com/ncb/bottomline/scores', decode=False)
     game = ""
     score = response.text_content()
-    raw=score.replace('%20',' ')
-    raw=raw.replace('^','')
-    raw=raw.replace('&','\n')
+    raw = score.replace('%20', ' ')
+    raw = raw.replace('^', '')
+    raw = raw.replace('&', '\n')
     pattern = re.compile("ncb_s_left\d+=(.*)")
     for match in re.findall(pattern, raw):
         if text.lower() in match.lower():
-            game = game +  match + "  "
+            game = game + match + "  "
     return(game)
 
 
 @hook.command("ncaaf", autohelp=False)
 def ncaafScores(text=" "):
     """ncaaf <team city> gets the score or next scheduled game for the specified team. If no team is specified all games will be included."""
-    response = http.get_html('http://scores.espn.go.com/ncf/bottomline/scores', decode=False)
+    response = http.get_html(
+        'http://scores.espn.go.com/ncf/bottomline/scores', decode=False)
     game = ""
     score = response.text_content()
-    raw=score.replace('%20',' ')
-    raw=raw.replace('^','')
-    raw=raw.replace('&','\n') 
-    raw=raw.replace('%26','&')
+    raw = score.replace('%20', ' ')
+    raw = raw.replace('^', '')
+    raw = raw.replace('&', '\n')
+    raw = raw.replace('%26', '&')
     pattern = re.compile("ncf_s_left\d+=(.*)")
     for match in re.findall(pattern, raw):
         if text.lower() in match.lower():
-            game = game +  match + "  "
+            game = game + match + "  "
     return(game)
+
 
 @hook.command("nhl", autohelp=False)
 def nhlScores(text=" "):
     """nhl <team city> gets the score or next scheduled game for the specified team. If no team is specified all games will be included."""
-    response = http.get_html('http://scores.espn.go.com/nhl/bottomline/scores', decode=False)
+    response = http.get_html(
+        'http://scores.espn.go.com/nhl/bottomline/scores', decode=False)
     game = ""
     score = response.text_content()
-    raw=score.replace('%20',' ')
-    raw=raw.replace('^','')
-    raw=raw.replace('&','\n')
+    raw = score.replace('%20', ' ')
+    raw = raw.replace('^', '')
+    raw = raw.replace('&', '\n')
     pattern = re.compile("nhl_s_left\d+=(.*)")
     for match in re.findall(pattern, raw):
         if text.lower() in match.lower():
-            game = game +  match + "  "
+            game = game + match + "  "
     return(game)
+
 
 @hook.command("wnba", autohelp=False)
 def wnbaScores(text=" "):
     """wnba <team city> gets the score or next scheduled game for the specified team. If no team is specified all games will be included."""
-    response = http.get_html('http://scores.espn.go.com/wnba/bottomline/scores', decode=False)
+    response = http.get_html(
+        'http://scores.espn.go.com/wnba/bottomline/scores', decode=False)
     game = ""
     score = response.text_content()
-    raw=score.replace('%20',' ')
-    raw=raw.replace('^','')
-    raw=raw.replace('&','\n')
+    raw = score.replace('%20', ' ')
+    raw = raw.replace('^', '')
+    raw = raw.replace('&', '\n')
     pattern = re.compile("wnba_s_left\d+=(.*)")
     for match in re.findall(pattern, raw):
         if text.lower() in match.lower():
-            game = game +  match + "  "
+            game = game + match + "  "
     return(game)

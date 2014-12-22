2c16a46345a83ccc7495ca68ba19a787aed5f085
Luke <lukeroge@gmail.com>
2014-12-23 07:34:23 +1300

    Added derpify from https://github.com/FurCode/RoboCop2/blob/robocop2/plugins/derpify.py - closes #18

--- plugins/utility.py
+++ plugins/utility.py
@@ -35,6 +35,12 @@ def strip(string):
     return strip_re.sub('', string)
 
 
+def translate(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+
 # onload
 @hook.onload()
 def load_text(bot):
@@ -170,6 +176,25 @@ def munge(text):
 def leet(text):
     output = ''.join(random.choice(leet[ch]) if ch.isalpha() else ch for ch in text.lower())
     return output
+
+
+@hook.command
+def derpify(text):
+    """derpify <text> - returns some amusing responses from your input."""
+    string = text.upper()
+    pick_the = random.choice(["TEH", "DA"])
+    pick_e = random.choice(["E", "3", "A"])
+    pick_qt = random.choice(["?!?!??", "???!!!!??", "?!??!?", "?!?!?!???"])
+    pick_ex = random.choice(["1111!11", "1!11", "!!1!", "1!!!!111", "!1!111!1", "!11!111"])
+    pick_end = random.choice(["", "OMG", "LOL", "WTF", "WTF LOL", "OMG LOL"])
+    rules = {"YOU'RE":"UR", "YOUR":"UR", "YOU":"U", "WHAT THE HECK":"WTH", "WHAT THE HELL":"WTH", "WHAT THE FUCK":"WTF",
+             "WHAT THE":"WT", "WHAT":"WUT", "ARE":"R", "WHY":"Y", "BE RIGHT BACK":"BRB", "BECAUSE":"B/C",
+             "OH MY GOD":"OMG", "O":"OH", "THE":pick_the, "TOO":"2", "TO":"2", "BE":"B", "CK":"K", "ING":"NG",
+             "PLEASE":"PLS", "SEE YOU":"CYA", "SEE YA":"CYA", "SCHOOL":"SKOOL", "AM":"M", "AM GOING TO":"IAM GOING TO",
+             "LIKE":"LIEK", "HELP":"HALP", "KE":"EK","E": pick_e, "!": pick_ex, "?": pick_qt}
+    output = translate(string, rules) + " " + pick_end
+
+    return output;
 
 
 # colors - based on code by Reece Selwood - <https://github.com/hitzler/homero>

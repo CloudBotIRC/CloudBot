dmptrluke/CloudbotX

Terminology changing

obrbot/bot.py
@@ -8,11 +8,11 @@ import gc
 import redis
 
 import obrbot
-from obrbot.client import Client
+from obrbot.connection import Connection
 from obrbot.config import Config
 from obrbot.plugin import PluginManager
 from obrbot.event import Event, CommandEvent, RegexEvent, EventType
-from obrbot.clients.irc import IrcClient
+from obrbot.clients.irc import IrcConnection
 
 logger = logging.getLogger("bot")
 
@@ -29,7 +29,7 @@ class ObrBot:
     """
     :type start_time: float
     :type running: bool
-    :type connections: list[Client | IrcClient]
+    :type connections: list[Connection | IrcConnection]
     :type config: core.config.Config
     :type plugin_manager: PluginManager
     :type db: redis.StrictRedis
@@ -100,7 +100,7 @@ class ObrBot:
             server = config['connection']['server']
             port = config['connection'].get('port', 6667)
 
-            self.connections.append(IrcClient(self, name, nick, config=config, channels=config.get('channels', []),
+            self.connections.append(IrcConnection(self, name, nick, config=config, channels=config.get('channels', []),
                                               readable_name=readable_name, server=server, port=port,
                                               use_ssl=config['connection'].get('ssl', False)))
             logger.debug("[{}] Created connection.".format(readable_name))
@@ -187,9 +187,9 @@ class ObrBot:
         if event.type is EventType.message:
             # Commands
             if event.chan.lower() == event.nick.lower():  # private message, no command prefix
-                command_re = r'(?i)^(?:[{}]?|{}[,;:]+\s+)(\w+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
+                command_re = r'(?i)^(?:[{}]?|{}[,;:]+\s+)([\w-]+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
             else:
-                command_re = r'(?i)^(?:[{}]|{}[,;:]+\s+)(\w+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
+                command_re = r'(?i)^(?:[{}]|{}[,;:]+\s+)([\w-]+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
 
             match = re.match(command_re, event.content)
 

obrbot/clients/irc.py
@@ -5,7 +5,7 @@ import ssl
 import logging
 from ssl import SSLContext
 
-from obrbot.client import Client
+from obrbot.connection import Connection
 from obrbot.event import Event, EventType
 
 logger = logging.getLogger("obrbot")
@@ -23,9 +23,9 @@ irc_command_to_event_type = {
 }
 
 
-class IrcClient(Client):
+class IrcConnection(Connection):
     """
-    An implementation of Client for IRC.
+    An implementation of Connection for IRC.
     :type use_ssl: bool
     :type server: str
     :type port: int
@@ -187,7 +187,7 @@ class IrcClient(Client):
         :type line: str
         """
         if not self._connected:
-            raise ValueError("Client must be connected to irc server to use send")
+            raise ValueError("Connection must be connected to irc server to use send")
         self.loop.call_soon_threadsafe(self._send, line, log_hide)
 
     def _send(self, line, log_hide):
@@ -205,11 +205,31 @@ class IrcClient(Client):
     def connected(self):
         return self._connected
 
+    @asyncio.coroutine
+    def process_waiting_regex_searches(self, event):
+        if event.type is not EventType.message:
+            return
+        for nick, chan, regex, future in self.waiting_messages:
+            if nick is not None:
+                if event.nick.lower() != nick:
+                    continue
+            if chan is not None:
+                if event.chan.lower() != chan:
+                    continue
+
+            try:
+                match = regex.search(event.content)
+            except Exception as exc:
+                future.set_exception(exc)
+            else:
+                if match:
+                    future.set_result(match)
+
 
 class _IrcProtocol(asyncio.Protocol):
     """
     :type loop: asyncio.events.AbstractEventLoop
-    :type conn: IrcClient
+    :type conn: IrcConnection
     :type bot: obrbot.bot.ObrBot
     :type _input_buffer: bytes
     :type _connected: bool
@@ -219,7 +239,7 @@ class _IrcProtocol(asyncio.Protocol):
 
     def __init__(self, conn):
         """
-        :type conn: IrcClient
+        :type conn: IrcConnection
         """
         self.loop = conn.loop
         self.bot = conn.bot
@@ -251,10 +271,7 @@ class _IrcProtocol(asyncio.Protocol):
         if exc is None:
             # we've been closed intentionally, so don't reconnect
             return
-        if exc is None:
-            logger.info("[{}] Connection lost.".format(self.conn.readable_name))
-        else:
-            logger.exception("[{}] Connection lost.".format(self.conn.readable_name))
+        logger.info("[{}] Connection lost.".format(self.conn.readable_name))
         asyncio.async(self.conn.connect(), loop=self.loop)
 
     def eof_received(self):
@@ -380,3 +397,4 @@ class _IrcProtocol(asyncio.Protocol):
 
             # handle the message, async
             asyncio.async(self.bot.process(event), loop=self.loop)
+            asyncio.async(self.conn.process_waiting_regex_searches(event), loop=self.loop)

obrbot/permissions.py
@@ -16,7 +16,7 @@ class PermissionManager(object):
 
     def __init__(self, conn):
         """
-        :type conn: obrbot.client.Client
+        :type conn: obrbot.connection.Connection
         """
         logger.info("[{}] Created permission manager for {}.".format(conn.readable_name, conn.name))
 

plugins/admin.py
@@ -8,7 +8,7 @@ from obrbot import hook
 @hook.command("groups", permissions=["permissions.view"], autohelp=False)
 def get_permission_groups(conn):
     """- lists all valid groups
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     """
     return "Valid groups: {}".format(conn.permissions.get_groups())
 
@@ -18,7 +18,7 @@ def get_permission_groups(conn):
 def get_group_permissions(text, conn, notice):
     """<group> - lists permissions given to <group>
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     """
     group = text.strip().lower()
     permission_manager = conn.permissions
@@ -37,7 +37,7 @@ def get_group_permissions(text, conn, notice):
 def get_group_users(text, conn, notice):
     """<group> - lists users in <group>
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     """
     group = text.strip().lower()
     permission_manager = conn.permissions
@@ -56,7 +56,7 @@ def get_group_users(text, conn, notice):
 def get_user_permissions(text, conn, mask, has_permission, notice):
     """[user] - lists all permissions given to [user], or the caller if no user is specified
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type mask: str
     """
     if text:
@@ -81,7 +81,7 @@ def get_user_permissions(text, conn, mask, has_permission, notice):
 def get_user_groups(text, conn, mask, has_permission, notice):
     """[user] - lists all permissions given to [user], or the caller if no user is specified
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type mask: str
     """
     if text:
@@ -107,7 +107,7 @@ def remove_permission_user(text, bot, conn, notice, reply):
     """<user> [group] - removes <user> from [group], or from all groups if no group is specified
     :type text: str
     :type bot: obrbot.bot.ObrBot
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     """
     split = text.split()
     if len(split) > 2:
@@ -162,7 +162,7 @@ def remove_permission_user(text, bot, conn, notice, reply):
 def add_permissions_user(text, conn, bot, notice, reply):
     """<user> <group> - adds <user> to <group>
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type bot: obrbot.bot.ObrBot
     """
     split = text.split()
@@ -230,7 +230,7 @@ def restart(text, bot):
 def join(text, conn, notice):
     """<channel> - joins <channel>
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     """
     for target in text.split():
         if not target.startswith("#"):
@@ -244,7 +244,7 @@ def join(text, conn, notice):
 def part(text, conn, chan, notice):
     """[#channel] - parts [#channel], or the caller's channel if no channel is specified
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type chan: str
     """
     if text:
@@ -263,7 +263,7 @@ def part(text, conn, chan, notice):
 def cycle(text, conn, chan, notice):
     """[#channel] - cycles [#channel], or the caller's channel if no channel is specified
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type chan: str
     """
     if text:
@@ -283,7 +283,7 @@ def cycle(text, conn, chan, notice):
 def nick(text, conn, notice):
     """<nick> - changes my nickname to <nick>
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     """
     if not re.match("^[a-z0-9_|.-\]\[]*$", text.lower()):
         notice("Invalid username '{}'".format(text))
@@ -297,7 +297,7 @@ def nick(text, conn, notice):
 def raw(text, conn, notice):
     """<command> - sends <command> as a raw IRC command
     :type text: str
-    :type conn: obrbot.clients.irc.IrcClient
+    :type conn: obrbot.clients.irc.IrcConnection
     """
     notice("Sending raw command.")
     conn.send(text)
@@ -308,7 +308,7 @@ def raw(text, conn, notice):
 def say(text, conn, chan):
     """[#channel] <message> - says <message> to [#channel], or to the caller's channel if no channel is specified
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type chan: str
     """
     text = text.strip()
@@ -327,7 +327,7 @@ def say(text, conn, chan):
 def message(text, conn):
     """<name> <message> - says <message> to <name>
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     """
     split = text.split(None, 1)
     channel = split[0]
@@ -340,7 +340,7 @@ def message(text, conn):
 def me(text, conn, chan):
     """[#channel] <action> - acts out <action> in a [#channel], or in the current channel of none is specified
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type chan: str
     """
     text = text.strip()

plugins/core_irc_login.py
@@ -10,7 +10,7 @@ logger = logging.getLogger('obrbot')
 @hook.irc_raw('004')
 def onjoin(conn):
     """
-    :type conn: obrbot.clients.irc.IrcClient
+    :type conn: obrbot.clients.irc.IrcConnection
     """
     nickserv = conn.config.get('nickserv')
     if nickserv and nickserv.get('enabled', True):
@@ -37,6 +37,8 @@ def onjoin(conn):
     # Join config-defined channels
     logger.info("Joining channels.")
     for channel in conn.channels:
+        if channel is None:
+            continue
         conn.join(channel)
         yield from asyncio.sleep(1)
 
@@ -47,7 +49,7 @@ def onjoin(conn):
 @hook.irc_raw('004')
 def keep_alive(conn):
     """
-    :type conn: obrbot.clients.irc.IrcClient
+    :type conn: obrbot.clients.irc.IrcConnection
     """
 
     if not conn.config.get('keep_alive', False):

plugins/core_tracker.py
@@ -16,7 +16,7 @@ nick_re = re.compile(":(.+?)!")
 @hook.event(EventType.kick)
 def on_kick(conn, chan, nick):
     """
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type chan: str
     :type nick: str
     """
@@ -32,7 +32,7 @@ def on_kick(conn, chan, nick):
 @hook.event(EventType.join)
 def on_join(conn, chan, nick):
     """
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type chan: str
     :type nick: str
     """
@@ -45,7 +45,7 @@ def on_join(conn, chan, nick):
 @hook.irc_raw("NICK")
 def on_nick(conn, irc_raw, content):
     """
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type irc_raw: str
     :type content: str
     """

plugins/help.py
@@ -10,7 +10,7 @@ from obrbot import hook
 def help_command(text, conn, bot, notice, has_permission):
     """[command] - gives help for [command], or lists all available commands if no command is specified
     :type text: str
-    :type conn: obrbot.client.Client
+    :type conn: obrbot.connection.Connection
     :type bot: obrbot.bot.ObrBot
     """
     if text:

CloudBotIRC/CloudBot

cloudbot/bot.py
@@ -8,11 +8,11 @@ import gc
 import redis
 
 import cloudbot
-from cloudbot.client import Client
+from cloudbot.connection import Connection
 from cloudbot.config import Config
 from cloudbot.plugin import PluginManager
 from cloudbot.event import Event, CommandEvent, RegexEvent, EventType
-from cloudbot.clients.irc import IrcClient
+from cloudbot.clients.irc import IrcConnection
 
 logger = logging.getLogger("bot")
 
@@ -29,7 +29,7 @@ class CloudBot:
     """
     :type start_time: float
     :type running: bool
-    :type connections: list[Client | IrcClient]
+    :type connections: list[Connection | IrcConnection]
     :type config: core.config.Config
     :type plugin_manager: PluginManager
     :type db: redis.StrictRedis
@@ -100,7 +100,7 @@ class CloudBot:
             server = config['connection']['server']
             port = config['connection'].get('port', 6667)
 
-            self.connections.append(IrcClient(self, name, nick, config=config, channels=config.get('channels', []),
+            self.connections.append(IrcConnection(self, name, nick, config=config, channels=config.get('channels', []),
                                               readable_name=readable_name, server=server, port=port,
                                               use_ssl=config['connection'].get('ssl', False)))
             logger.debug("[{}] Created connection.".format(readable_name))
@@ -187,9 +187,9 @@ class CloudBot:
         if event.type is EventType.message:
             # Commands
             if event.chan.lower() == event.nick.lower():  # private message, no command prefix
-                command_re = r'(?i)^(?:[{}]?|{}[,;:]+\s+)(\w+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
+                command_re = r'(?i)^(?:[{}]?|{}[,;:]+\s+)([\w-]+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
             else:
-                command_re = r'(?i)^(?:[{}]|{}[,;:]+\s+)(\w+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
+                command_re = r'(?i)^(?:[{}]|{}[,;:]+\s+)([\w-]+)(?:$|\s+)(.*)'.format(command_prefix, event.conn.nick)
 
             match = re.match(command_re, event.content)
 

cloudbot/clients/irc.py
@@ -5,7 +5,7 @@ import ssl
 import logging
 from ssl import SSLContext
 
-from cloudbot.client import Client
+from cloudbot.connection import Connection
 from cloudbot.event import Event, EventType
 
 logger = logging.getLogger("cloudbot")
@@ -23,9 +23,9 @@ irc_command_to_event_type = {
 }
 
 
-class IrcClient(Client):
+class IrcConnection(Connection):
     """
-    An implementation of Client for IRC.
+    An implementation of Connection for IRC.
     :type use_ssl: bool
     :type server: str
     :type port: int
@@ -187,7 +187,7 @@ class IrcClient(Client):
         :type line: str
         """
         if not self._connected:
-            raise ValueError("Client must be connected to irc server to use send")
+            raise ValueError("Connection must be connected to irc server to use send")
         self.loop.call_soon_threadsafe(self._send, line, log_hide)
 
     def _send(self, line, log_hide):
@@ -205,11 +205,31 @@ class IrcClient(Client):
     def connected(self):
         return self._connected
 
+    @asyncio.coroutine
+    def process_waiting_regex_searches(self, event):
+        if event.type is not EventType.message:
+            return
+        for nick, chan, regex, future in self.waiting_messages:
+            if nick is not None:
+                if event.nick.lower() != nick:
+                    continue
+            if chan is not None:
+                if event.chan.lower() != chan:
+                    continue
+
+            try:
+                match = regex.search(event.content)
+            except Exception as exc:
+                future.set_exception(exc)
+            else:
+                if match:
+                    future.set_result(match)
+
 
 class _IrcProtocol(asyncio.Protocol):
     """
     :type loop: asyncio.events.AbstractEventLoop
-    :type conn: IrcClient
+    :type conn: IrcConnection
     :type bot: cloudbot.bot.CloudBot
     :type _input_buffer: bytes
     :type _connected: bool
@@ -219,7 +239,7 @@ class _IrcProtocol(asyncio.Protocol):
 
     def __init__(self, conn):
         """
-        :type conn: IrcClient
+        :type conn: IrcConnection
         """
         self.loop = conn.loop
         self.bot = conn.bot
@@ -251,10 +271,7 @@ class _IrcProtocol(asyncio.Protocol):
         if exc is None:
             # we've been closed intentionally, so don't reconnect
             return
-        if exc is None:
-            logger.info("[{}] Connection lost.".format(self.conn.readable_name))
-        else:
-            logger.exception("[{}] Connection lost.".format(self.conn.readable_name))
+        logger.info("[{}] Connection lost.".format(self.conn.readable_name))
         asyncio.async(self.conn.connect(), loop=self.loop)
 
     def eof_received(self):
@@ -380,3 +397,4 @@ class _IrcProtocol(asyncio.Protocol):
 
             # handle the message, async
             asyncio.async(self.bot.process(event), loop=self.loop)
+            asyncio.async(self.conn.process_waiting_regex_searches(event), loop=self.loop)

cloudbot/permissions.py
@@ -16,7 +16,7 @@ class PermissionManager(object):
 
     def __init__(self, conn):
         """
-        :type conn: cloudbot.client.Client
+        :type conn: cloudbot.connection.Connection
         """
         logger.info("[{}] Created permission manager for {}.".format(conn.readable_name, conn.name))
 

plugins/admin.py
@@ -8,7 +8,7 @@ from cloudbot import hook
 @hook.command("groups", permissions=["permissions.view"], autohelp=False)
 def get_permission_groups(conn):
     """- lists all valid groups
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     """
     return "Valid groups: {}".format(conn.permissions.get_groups())
 
@@ -18,7 +18,7 @@ def get_permission_groups(conn):
 def get_group_permissions(text, conn, notice):
     """<group> - lists permissions given to <group>
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     """
     group = text.strip().lower()
     permission_manager = conn.permissions
@@ -37,7 +37,7 @@ def get_group_permissions(text, conn, notice):
 def get_group_users(text, conn, notice):
     """<group> - lists users in <group>
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     """
     group = text.strip().lower()
     permission_manager = conn.permissions
@@ -56,7 +56,7 @@ def get_group_users(text, conn, notice):
 def get_user_permissions(text, conn, mask, has_permission, notice):
     """[user] - lists all permissions given to [user], or the caller if no user is specified
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type mask: str
     """
     if text:
@@ -81,7 +81,7 @@ def get_user_permissions(text, conn, mask, has_permission, notice):
 def get_user_groups(text, conn, mask, has_permission, notice):
     """[user] - lists all permissions given to [user], or the caller if no user is specified
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type mask: str
     """
     if text:
@@ -107,7 +107,7 @@ def remove_permission_user(text, bot, conn, notice, reply):
     """<user> [group] - removes <user> from [group], or from all groups if no group is specified
     :type text: str
     :type bot: cloudbot.bot.CloudBot
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     """
     split = text.split()
     if len(split) > 2:
@@ -162,7 +162,7 @@ def remove_permission_user(text, bot, conn, notice, reply):
 def add_permissions_user(text, conn, bot, notice, reply):
     """<user> <group> - adds <user> to <group>
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type bot: cloudbot.bot.CloudBot
     """
     split = text.split()
@@ -230,7 +230,7 @@ def restart(text, bot):
 def join(text, conn, notice):
     """<channel> - joins <channel>
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     """
     for target in text.split():
         if not target.startswith("#"):
@@ -244,7 +244,7 @@ def join(text, conn, notice):
 def part(text, conn, chan, notice):
     """[#channel] - parts [#channel], or the caller's channel if no channel is specified
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type chan: str
     """
     if text:
@@ -263,7 +263,7 @@ def part(text, conn, chan, notice):
 def cycle(text, conn, chan, notice):
     """[#channel] - cycles [#channel], or the caller's channel if no channel is specified
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type chan: str
     """
     if text:
@@ -283,7 +283,7 @@ def cycle(text, conn, chan, notice):
 def nick(text, conn, notice):
     """<nick> - changes my nickname to <nick>
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     """
     if not re.match("^[a-z0-9_|.-\]\[]*$", text.lower()):
         notice("Invalid username '{}'".format(text))
@@ -297,7 +297,7 @@ def nick(text, conn, notice):
 def raw(text, conn, notice):
     """<command> - sends <command> as a raw IRC command
     :type text: str
-    :type conn: cloudbot.clients.irc.IrcClient
+    :type conn: cloudbot.clients.irc.IrcConnection
     """
     notice("Sending raw command.")
     conn.send(text)
@@ -308,7 +308,7 @@ def raw(text, conn, notice):
 def say(text, conn, chan):
     """[#channel] <message> - says <message> to [#channel], or to the caller's channel if no channel is specified
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type chan: str
     """
     text = text.strip()
@@ -327,7 +327,7 @@ def say(text, conn, chan):
 def message(text, conn):
     """<name> <message> - says <message> to <name>
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     """
     split = text.split(None, 1)
     channel = split[0]
@@ -340,7 +340,7 @@ def message(text, conn):
 def me(text, conn, chan):
     """[#channel] <action> - acts out <action> in a [#channel], or in the current channel of none is specified
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type chan: str
     """
     text = text.strip()

plugins/core_irc_login.py
@@ -10,7 +10,7 @@ logger = logging.getLogger('cloudbot')
 @hook.irc_raw('004')
 def onjoin(conn):
     """
-    :type conn: cloudbot.clients.irc.IrcClient
+    :type conn: cloudbot.clients.irc.IrcConnection
     """
     nickserv = conn.config.get('nickserv')
     if nickserv and nickserv.get('enabled', True):
@@ -37,6 +37,8 @@ def onjoin(conn):
     # Join config-defined channels
     logger.info("Joining channels.")
     for channel in conn.channels:
+        if channel is None:
+            continue
         conn.join(channel)
         yield from asyncio.sleep(1)
 
@@ -47,7 +49,7 @@ def onjoin(conn):
 @hook.irc_raw('004')
 def keep_alive(conn):
     """
-    :type conn: cloudbot.clients.irc.IrcClient
+    :type conn: cloudbot.clients.irc.IrcConnection
     """
 
     if not conn.config.get('keep_alive', False):

plugins/core_tracker.py
@@ -16,7 +16,7 @@ nick_re = re.compile(":(.+?)!")
 @hook.event(EventType.kick)
 def on_kick(conn, chan, nick):
     """
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type chan: str
     :type nick: str
     """
@@ -32,7 +32,7 @@ def on_kick(conn, chan, nick):
 @hook.event(EventType.join)
 def on_join(conn, chan, nick):
     """
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type chan: str
     :type nick: str
     """
@@ -45,7 +45,7 @@ def on_join(conn, chan, nick):
 @hook.irc_raw("NICK")
 def on_nick(conn, irc_raw, content):
     """
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type irc_raw: str
     :type content: str
     """

plugins/help.py
@@ -10,7 +10,7 @@ from cloudbot import hook
 def help_command(text, conn, bot, notice, has_permission):
     """[command] - gives help for [command], or lists all available commands if no command is specified
     :type text: str
-    :type conn: cloudbot.client.Client
+    :type conn: cloudbot.connection.Connection
     :type bot: cloudbot.bot.CloudBot
     """
     if text:
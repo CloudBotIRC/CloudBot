dmptrluke/CloudbotX

Remove logger from event

plugins/core_irc_login.py
@@ -1,18 +1,19 @@
 import asyncio
+import logging
 
 from obrbot import hook
 
+logger = logging.getLogger('obrbot')
 
 # Identify to NickServ (or other service)
 @asyncio.coroutine
 @hook.irc_raw('004')
-def onjoin(conn, logger):
+def onjoin(conn):
     """
     :type conn: obrbot.clients.irc.IrcClient
-    :type logger: logging.Logger
     """
     nickserv = conn.config.get('nickserv')
-    if nickserv and nickserv.get("enabled", True):
+    if nickserv and nickserv.get('enabled', True):
         nickserv_password = nickserv.get('nickserv_password', '')
         nickserv_name = nickserv.get('nickserv_name', 'nickserv')
         nickserv_account_name = nickserv.get('nickserv_user', '')
@@ -30,11 +31,11 @@ def onjoin(conn, logger):
     # Set bot modes
     mode = conn.config.get('mode')
     if mode:
-        logger.info('Setting bot mode: "{}"'.format(mode))
+        logger.info("Setting bot mode: '{}'".format(mode))
         conn.cmd('MODE', conn.nick, mode)
 
     # Join config-defined channels
-    logger.info('Joining channels.')
+    logger.info("Joining channels.")
     for channel in conn.channels:
         conn.join(channel)
         yield from asyncio.sleep(1)

plugins/core_tracker.py
@@ -7,7 +7,7 @@ import re
 from obrbot import hook
 from obrbot.event import EventType
 
-logger = logging.getLogger("obrbot")
+logger = logging.getLogger('obrbot')
 
 nick_re = re.compile(":(.+?)!")
 
@@ -45,9 +45,9 @@ def on_join(conn, chan, nick):
 @hook.irc_raw("NICK")
 def on_nick(conn, irc_raw, content):
     """
-    :type irc_paramlist: list[str]
     :type conn: obrbot.client.Client
     :type irc_raw: str
+    :type content: str
     """
     old_nick = nick_re.search(irc_raw).group(1)
     if old_nick == conn.nick:

plugins/log.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 import codecs
 import time
@@ -8,6 +9,8 @@ import itertools
 import obrbot
 from obrbot import hook
 from obrbot.event import EventType
+
+logger = logging.getLogger('obrbot')
 
 irc_color_re = re.compile(r"(\x03(\d+,\d+|\d)|[\x0f\x02\x16\x1f])")
 
@@ -229,10 +232,9 @@ def log(event):
 # Log console separately to prevent lag
 @asyncio.coroutine
 @hook.irc_raw("*", run_first=True)
-def console_log(event, logger):
+def console_log(event):
     """
     :type event: obrbot.event.Event
-    :type logger: logging.Logger
     """
     text = format_event(event)
     if text is not None:

CloudBotIRC/CloudBot

plugins/core_irc_login.py
@@ -1,18 +1,19 @@
 import asyncio
+import logging
 
 from cloudbot import hook
 
+logger = logging.getLogger('cloudbot')
 
 # Identify to NickServ (or other service)
 @asyncio.coroutine
 @hook.irc_raw('004')
-def onjoin(conn, logger):
+def onjoin(conn):
     """
     :type conn: cloudbot.clients.irc.IrcClient
-    :type logger: logging.Logger
     """
     nickserv = conn.config.get('nickserv')
-    if nickserv and nickserv.get("enabled", True):
+    if nickserv and nickserv.get('enabled', True):
         nickserv_password = nickserv.get('nickserv_password', '')
         nickserv_name = nickserv.get('nickserv_name', 'nickserv')
         nickserv_account_name = nickserv.get('nickserv_user', '')
@@ -30,11 +31,11 @@ def onjoin(conn, logger):
     # Set bot modes
     mode = conn.config.get('mode')
     if mode:
-        logger.info('Setting bot mode: "{}"'.format(mode))
+        logger.info("Setting bot mode: '{}'".format(mode))
         conn.cmd('MODE', conn.nick, mode)
 
     # Join config-defined channels
-    logger.info('Joining channels.')
+    logger.info("Joining channels.")
     for channel in conn.channels:
         conn.join(channel)
         yield from asyncio.sleep(1)

plugins/core_tracker.py
@@ -7,7 +7,7 @@ import re
 from cloudbot import hook
 from cloudbot.event import EventType
 
-logger = logging.getLogger("cloudbot")
+logger = logging.getLogger('cloudbot')
 
 nick_re = re.compile(":(.+?)!")
 
@@ -45,9 +45,9 @@ def on_join(conn, chan, nick):
 @hook.irc_raw("NICK")
 def on_nick(conn, irc_raw, content):
     """
-    :type irc_paramlist: list[str]
     :type conn: cloudbot.client.Client
     :type irc_raw: str
+    :type content: str
     """
     old_nick = nick_re.search(irc_raw).group(1)
     if old_nick == conn.nick:

plugins/log.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 import codecs
 import time
@@ -8,6 +9,8 @@ import itertools
 import cloudbot
 from cloudbot import hook
 from cloudbot.event import EventType
+
+logger = logging.getLogger('cloudbot')
 
 irc_color_re = re.compile(r"(\x03(\d+,\d+|\d)|[\x0f\x02\x16\x1f])")
 
@@ -229,10 +232,9 @@ def log(event):
 # Log console separately to prevent lag
 @asyncio.coroutine
 @hook.irc_raw("*", run_first=True)
-def console_log(event, logger):
+def console_log(event):
     """
     :type event: cloudbot.event.Event
-    :type logger: logging.Logger
     """
     text = format_event(event)
     if text is not None:
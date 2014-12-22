dmptrluke/CloudbotX

Change a bunch of stuff, probably unnecessarily

obrbot/bot.py
@@ -2,7 +2,6 @@ import asyncio
 import time
 import logging
 import re
-import os
 import gc
 
 import redis
@@ -10,7 +9,7 @@ import redis
 from obrbot.connection import Connection
 from obrbot.config import Config
 from obrbot.plugin import PluginManager
-from obrbot.event import Event, CommandEvent, RegexEvent, EventType
+from obrbot.event import Event, CommandHookEvent, RegexHookEvent, EventType
 from obrbot.clients.irc import IrcConnection
 
 logger = logging.getLogger("bot")
@@ -154,27 +153,27 @@ class ObrBot:
         tasks = []
         command_prefix = event.conn.config.get('command_prefix', '.')
 
-        # Raw IRC hook
-        for raw_hook in self.plugin_manager.catch_all_triggers:
-            # run catch-all coroutine hooks before all others - TODO: Make this a plugin argument
-            if raw_hook.run_first:
-                first.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
-            else:
-                tasks.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
-        if event.irc_command in self.plugin_manager.raw_triggers:
-            for raw_hook in self.plugin_manager.raw_triggers[event.irc_command]:
+        if hasattr(event, 'irc_command'):
+            # Raw IRC hook
+            for raw_hook in self.plugin_manager.catch_all_triggers:
                 if raw_hook.run_first:
-                    first.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
+                    first.append(self.plugin_manager.launch(raw_hook, event))
                 else:
-                    tasks.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
+                    tasks.append(self.plugin_manager.launch(raw_hook, event))
+            if event.irc_command in self.plugin_manager.raw_triggers:
+                for raw_hook in self.plugin_manager.raw_triggers[event.irc_command]:
+                    if raw_hook.run_first:
+                        first.append(self.plugin_manager.launch(raw_hook, event))
+                    else:
+                        tasks.append(self.plugin_manager.launch(raw_hook, event))
 
         # Event hooks
         if event.type in self.plugin_manager.event_type_hooks:
             for event_hook in self.plugin_manager.event_type_hooks[event.type]:
                 if event_hook.run_first:
-                    first.append(self.plugin_manager.launch(event_hook, Event(hook=event_hook, base_event=event)))
+                    first.append(self.plugin_manager.launch(event_hook, event))
                 else:
-                    tasks.append(self.plugin_manager.launch(event_hook, Event(hook=event_hook, base_event=event)))
+                    tasks.append(self.plugin_manager.launch(event_hook, event))
 
         if event.type is EventType.message:
             # Commands
@@ -191,22 +190,22 @@ class ObrBot:
                 command = match.group(1).lower()
                 if command in self.plugin_manager.commands:
                     command_hook = self.plugin_manager.commands[command]
-                    command_event = CommandEvent(hook=command_hook, text=match.group(2).strip(),
-                                                 triggered_command=command, base_event=event)
+                    command_event = CommandHookEvent(hook=command_hook, text=match.group(2).strip(),
+                                                     triggered_command=command, base_event=event)
                     if command_hook.run_first:
-                        first.append(self.plugin_manager.launch(command_hook, command_event))
+                        first.append(self.plugin_manager.launch(command_hook, event, command_event))
                     else:
-                        tasks.append(self.plugin_manager.launch(command_hook, command_event))
+                        tasks.append(self.plugin_manager.launch(command_hook, event, command_event))
 
             # Regex hooks
             for regex, regex_hook in self.plugin_manager.regex_hooks:
                 match = regex.search(event.content)
                 if match:
-                    regex_event = RegexEvent(hook=regex_hook, match=match, base_event=event)
+                    regex_event = RegexHookEvent(hook=regex_hook, match=match, base_event=event)
                     if regex_hook.run_first:
-                        first.append(self.plugin_manager.launch(regex_hook, regex_event))
+                        first.append(self.plugin_manager.launch(regex_hook, event, regex_event))
                     else:
-                        tasks.append(self.plugin_manager.launch(regex_hook, regex_event))
+                        tasks.append(self.plugin_manager.launch(regex_hook, event, regex_event))
 
         # Run the tasks
         yield from asyncio.gather(*first, loop=self.loop)

obrbot/clients/irc.py
@@ -6,7 +6,7 @@ import logging
 from ssl import SSLContext
 
 from obrbot.connection import Connection, Channel
-from obrbot.event import Event, EventType
+from obrbot.event import Event, EventType, IrcEvent
 
 logger = logging.getLogger("obrbot")
 
@@ -418,9 +418,9 @@ class _IrcProtocol(asyncio.Protocol):
                 target = None
 
             # Set up parsed message
-            event = Event(bot=self.bot, conn=self.conn, event_type=event_type, content=content, target=target,
-                          channel_name=channel, nick=nick, user=user, host=host, mask=mask, irc_raw=line,
-                          irc_command=command, irc_command_params=command_params, irc_ctcp_text=ctcp_text)
+            event = IrcEvent(bot=self.bot, conn=self.conn, event_type=event_type, content=content, target=target,
+                             channel_name=channel, nick=nick, user=user, host=host, mask=mask, irc_raw=line,
+                             irc_command=command, irc_command_params=command_params, irc_ctcp_text=ctcp_text)
             asyncio.async(self.process(event))
 
     @asyncio.coroutine

plugins/ignore.py
@@ -15,13 +15,14 @@ logger = logging.getLogger("obrbot")
 
 @asyncio.coroutine
 @hook.sieve()
-def ignore_sieve(event):
+def ignore_sieve(event, hook_event):
     """ blocks events from ignored channels/hosts
     :type event: obrbot.event.Event
+    :type hook_event: obrbot.event.HookEvent
     """
     bot = event.bot
     # don't block event hooks
-    if event.hook.type is HookType.event or event.hook.type is HookType.irc_raw:
+    if hook_event.hook.type is HookType.event or hook_event.hook.type is HookType.irc_raw:
         return event
 
     # don't block server messages
@@ -29,7 +30,7 @@ def ignore_sieve(event):
         return event
 
     # don't block an event that could be un-ignoring
-    if event.hook.type is HookType.command and event.hook.function_name == 'unignore':
+    if hook_event.hook.type is HookType.command and hook_event.hook.function_name == 'unignore':
         return event
 
     ignore_list = yield from event.async(bot.db.smembers, 'plugins:ignore:ignored')
@@ -39,11 +40,11 @@ def ignore_sieve(event):
         pattern = pattern.decode()
         if pattern.startswith('#'):
             if fnmatch(event.chan_name, pattern):
-                logger.info("Ignoring {}: Skipping hook {}".format(event.chan_name, event.hook.description))
+                logger.info("Ignoring {}: Skipping hook {}".format(event.chan_name, hook_event.hook.description))
                 return None
         else:
             if fnmatch(mask, pattern):
-                logger.info("Ignoring {}: Skipping hook {}".format(event.mask, event.hook.description))
+                logger.info("Ignoring {}: Skipping hook {}".format(event.mask, hook_event.hook.description))
                 return None
 
     return event

plugins/log.py
@@ -65,7 +65,7 @@ ctcp_known_with_message = ("[{server}:{channel}] {nick} [{user}@{host}] "
 def format_event(event):
     """
     Format an event
-    :type event: obrbot.event.Event
+    :type event: obrbot.event.Event | obrbot.event.IrcEvent
     :rtype: str
     """
 
@@ -88,14 +88,14 @@ def format_event(event):
         return base_formats[event.type].format(**args)
 
     # Try formatting with IRC-formats, if this is an IRC event
-    if event.irc_command is not None:
+    if hasattr(event, 'irc_command'):
         return format_irc_event(event, args)
 
 
 def format_irc_event(event, args):
     """
     Format an IRC event
-    :type event: obrbot.event.Event
+    :type event: obrbot.event.IrcEvent
     :param event: The event to format
     :param args: The pre-created arguments
     """
@@ -215,7 +215,7 @@ def get_raw_log_stream(server):
 @hook.irc_raw("*", single_instance=True)
 def log_raw(event):
     """
-    :type event: obrbot.event.Event
+    :type event: obrbot.event.IrcEvent
     """
     logging_config = event.bot.config.get('logging', {})
     if not logging_config.get("raw_file_log", False):
@@ -227,7 +227,7 @@ def log_raw(event):
 @hook.irc_raw("*", single_instance=True)
 def log(event):
     """
-    :type event: obrbot.event.Event
+    :type event: obrbot.event.IrcEvent
     """
     text = format_event(event)
 

plugins/plugin_sieve.py
@@ -17,13 +17,14 @@ channel_buckets = {}
 
 @asyncio.coroutine
 @hook.sieve()
-def sieve_suite(event):
+def sieve_suite(event, hook_event):
     """
     :type event: obrbot.event.Event
+    :type hook_event: obrbot.event.HookEvent
     """
 
     # check permissions
-    allowed_permissions = event.hook.permissions
+    allowed_permissions = hook_event.hook.permissions
     if allowed_permissions:
         for perm in allowed_permissions:
             if event.has_permission(perm):
@@ -33,7 +34,7 @@ def sieve_suite(event):
             return None
 
     # check command spam tokens
-    if event.hook.type is HookType.command:
+    if hook_event.hook.type is HookType.command:
         if not event.chan_name in channel_buckets:
             _bucket = bucket.TokenBucket(command_limiting_initial_tokens, command_limiting_restore_rate)
             channel_buckets[event.chan_name] = _bucket

CloudBotIRC/CloudBot

cloudbot/bot.py
@@ -2,7 +2,6 @@ import asyncio
 import time
 import logging
 import re
-import os
 import gc
 
 import redis
@@ -10,7 +9,7 @@ import redis
 from cloudbot.connection import Connection
 from cloudbot.config import Config
 from cloudbot.plugin import PluginManager
-from cloudbot.event import Event, CommandEvent, RegexEvent, EventType
+from cloudbot.event import Event, CommandHookEvent, RegexHookEvent, EventType
 from cloudbot.clients.irc import IrcConnection
 
 logger = logging.getLogger("bot")
@@ -154,27 +153,27 @@ class CloudBot:
         tasks = []
         command_prefix = event.conn.config.get('command_prefix', '.')
 
-        # Raw IRC hook
-        for raw_hook in self.plugin_manager.catch_all_triggers:
-            # run catch-all coroutine hooks before all others - TODO: Make this a plugin argument
-            if raw_hook.run_first:
-                first.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
-            else:
-                tasks.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
-        if event.irc_command in self.plugin_manager.raw_triggers:
-            for raw_hook in self.plugin_manager.raw_triggers[event.irc_command]:
+        if hasattr(event, 'irc_command'):
+            # Raw IRC hook
+            for raw_hook in self.plugin_manager.catch_all_triggers:
                 if raw_hook.run_first:
-                    first.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
+                    first.append(self.plugin_manager.launch(raw_hook, event))
                 else:
-                    tasks.append(self.plugin_manager.launch(raw_hook, Event(hook=raw_hook, base_event=event)))
+                    tasks.append(self.plugin_manager.launch(raw_hook, event))
+            if event.irc_command in self.plugin_manager.raw_triggers:
+                for raw_hook in self.plugin_manager.raw_triggers[event.irc_command]:
+                    if raw_hook.run_first:
+                        first.append(self.plugin_manager.launch(raw_hook, event))
+                    else:
+                        tasks.append(self.plugin_manager.launch(raw_hook, event))
 
         # Event hooks
         if event.type in self.plugin_manager.event_type_hooks:
             for event_hook in self.plugin_manager.event_type_hooks[event.type]:
                 if event_hook.run_first:
-                    first.append(self.plugin_manager.launch(event_hook, Event(hook=event_hook, base_event=event)))
+                    first.append(self.plugin_manager.launch(event_hook, event))
                 else:
-                    tasks.append(self.plugin_manager.launch(event_hook, Event(hook=event_hook, base_event=event)))
+                    tasks.append(self.plugin_manager.launch(event_hook, event))
 
         if event.type is EventType.message:
             # Commands
@@ -191,22 +190,22 @@ class CloudBot:
                 command = match.group(1).lower()
                 if command in self.plugin_manager.commands:
                     command_hook = self.plugin_manager.commands[command]
-                    command_event = CommandEvent(hook=command_hook, text=match.group(2).strip(),
-                                                 triggered_command=command, base_event=event)
+                    command_event = CommandHookEvent(hook=command_hook, text=match.group(2).strip(),
+                                                     triggered_command=command, base_event=event)
                     if command_hook.run_first:
-                        first.append(self.plugin_manager.launch(command_hook, command_event))
+                        first.append(self.plugin_manager.launch(command_hook, event, command_event))
                     else:
-                        tasks.append(self.plugin_manager.launch(command_hook, command_event))
+                        tasks.append(self.plugin_manager.launch(command_hook, event, command_event))
 
             # Regex hooks
             for regex, regex_hook in self.plugin_manager.regex_hooks:
                 match = regex.search(event.content)
                 if match:
-                    regex_event = RegexEvent(hook=regex_hook, match=match, base_event=event)
+                    regex_event = RegexHookEvent(hook=regex_hook, match=match, base_event=event)
                     if regex_hook.run_first:
-                        first.append(self.plugin_manager.launch(regex_hook, regex_event))
+                        first.append(self.plugin_manager.launch(regex_hook, event, regex_event))
                     else:
-                        tasks.append(self.plugin_manager.launch(regex_hook, regex_event))
+                        tasks.append(self.plugin_manager.launch(regex_hook, event, regex_event))
 
         # Run the tasks
         yield from asyncio.gather(*first, loop=self.loop)

cloudbot/clients/irc.py
@@ -6,7 +6,7 @@ import logging
 from ssl import SSLContext
 
 from cloudbot.connection import Connection, Channel
-from cloudbot.event import Event, EventType
+from cloudbot.event import Event, EventType, IrcEvent
 
 logger = logging.getLogger("cloudbot")
 
@@ -418,9 +418,9 @@ class _IrcProtocol(asyncio.Protocol):
                 target = None
 
             # Set up parsed message
-            event = Event(bot=self.bot, conn=self.conn, event_type=event_type, content=content, target=target,
-                          channel_name=channel, nick=nick, user=user, host=host, mask=mask, irc_raw=line,
-                          irc_command=command, irc_command_params=command_params, irc_ctcp_text=ctcp_text)
+            event = IrcEvent(bot=self.bot, conn=self.conn, event_type=event_type, content=content, target=target,
+                             channel_name=channel, nick=nick, user=user, host=host, mask=mask, irc_raw=line,
+                             irc_command=command, irc_command_params=command_params, irc_ctcp_text=ctcp_text)
             asyncio.async(self.process(event))
 
     @asyncio.coroutine

plugins/ignore.py
@@ -15,13 +15,14 @@ logger = logging.getLogger("cloudbot")
 
 @asyncio.coroutine
 @hook.sieve()
-def ignore_sieve(event):
+def ignore_sieve(event, hook_event):
     """ blocks events from ignored channels/hosts
     :type event: cloudbot.event.Event
+    :type hook_event: cloudbot.event.HookEvent
     """
     bot = event.bot
     # don't block event hooks
-    if event.hook.type is HookType.event or event.hook.type is HookType.irc_raw:
+    if hook_event.hook.type is HookType.event or hook_event.hook.type is HookType.irc_raw:
         return event
 
     # don't block server messages
@@ -29,7 +30,7 @@ def ignore_sieve(event):
         return event
 
     # don't block an event that could be un-ignoring
-    if event.hook.type is HookType.command and event.hook.function_name == 'unignore':
+    if hook_event.hook.type is HookType.command and hook_event.hook.function_name == 'unignore':
         return event
 
     ignore_list = yield from event.async(bot.db.smembers, 'plugins:ignore:ignored')
@@ -39,11 +40,11 @@ def ignore_sieve(event):
         pattern = pattern.decode()
         if pattern.startswith('#'):
             if fnmatch(event.chan_name, pattern):
-                logger.info("Ignoring {}: Skipping hook {}".format(event.chan_name, event.hook.description))
+                logger.info("Ignoring {}: Skipping hook {}".format(event.chan_name, hook_event.hook.description))
                 return None
         else:
             if fnmatch(mask, pattern):
-                logger.info("Ignoring {}: Skipping hook {}".format(event.mask, event.hook.description))
+                logger.info("Ignoring {}: Skipping hook {}".format(event.mask, hook_event.hook.description))
                 return None
 
     return event

plugins/log.py
@@ -65,7 +65,7 @@ ctcp_known_with_message = ("[{server}:{channel}] {nick} [{user}@{host}] "
 def format_event(event):
     """
     Format an event
-    :type event: cloudbot.event.Event
+    :type event: cloudbot.event.Event | cloudbot.event.IrcEvent
     :rtype: str
     """
 
@@ -88,14 +88,14 @@ def format_event(event):
         return base_formats[event.type].format(**args)
 
     # Try formatting with IRC-formats, if this is an IRC event
-    if event.irc_command is not None:
+    if hasattr(event, 'irc_command'):
         return format_irc_event(event, args)
 
 
 def format_irc_event(event, args):
     """
     Format an IRC event
-    :type event: cloudbot.event.Event
+    :type event: cloudbot.event.IrcEvent
     :param event: The event to format
     :param args: The pre-created arguments
     """
@@ -215,7 +215,7 @@ def get_raw_log_stream(server):
 @hook.irc_raw("*", single_instance=True)
 def log_raw(event):
     """
-    :type event: cloudbot.event.Event
+    :type event: cloudbot.event.IrcEvent
     """
     logging_config = event.bot.config.get('logging', {})
     if not logging_config.get("raw_file_log", False):
@@ -227,7 +227,7 @@ def log_raw(event):
 @hook.irc_raw("*", single_instance=True)
 def log(event):
     """
-    :type event: cloudbot.event.Event
+    :type event: cloudbot.event.IrcEvent
     """
     text = format_event(event)
 

plugins/plugin_sieve.py
@@ -17,13 +17,14 @@ channel_buckets = {}
 
 @asyncio.coroutine
 @hook.sieve()
-def sieve_suite(event):
+def sieve_suite(event, hook_event):
     """
     :type event: cloudbot.event.Event
+    :type hook_event: cloudbot.event.HookEvent
     """
 
     # check permissions
-    allowed_permissions = event.hook.permissions
+    allowed_permissions = hook_event.hook.permissions
     if allowed_permissions:
         for perm in allowed_permissions:
             if event.has_permission(perm):
@@ -33,7 +34,7 @@ def sieve_suite(event):
             return None
 
     # check command spam tokens
-    if event.hook.type is HookType.command:
+    if hook_event.hook.type is HookType.command:
         if not event.chan_name in channel_buckets:
             _bucket = bucket.TokenBucket(command_limiting_initial_tokens, command_limiting_restore_rate)
             channel_buckets[event.chan_name] = _bucket
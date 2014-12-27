c76fe4e3204d7362776f7ad90e27317e0061783c
Luke <lukeroge@gmail.com>
2014-12-28 00:10:49 +1300

    hacky new steamid thingy

---
+++ plugins/steam_user.py
@@ -0,0 +1,84 @@
+import requests
+from lxml import etree
+
+from cloudbot import hook
+
+from cloudbot.util import formatting
+
+
+API_URL = "http://steamcommunity.com/id/{}/"
+ID_BASE = 76561197960265728
+
+
+class SteamError(Exception):
+    pass
+
+
+def convert_id32(id_64):
+    steamID = []
+    steamID.append("STEAM_0:")
+    steamIDLastPart = id_64 - ID_BASE
+    if steamIDLastPart % 2 == 0:
+        steamID.append("0:")
+    else:
+        steamID.append("1:")
+    steamID.append(str(steamIDLastPart // 2))
+    return "".join(steamID)
+
+
+def convert_id3(id_64):
+    _id = (id_64 - ID_BASE) * 2
+    if _id % 2 == 0:
+        _id += 0
+    else:
+        _id += 1
+    actual = str(_id // 2)
+    return "U:1:{}".format(actual)
+
+
+def get_data(user):
+    """
+    takes a steam user ID and returns a dict containing info about the user
+    :type user: str
+    :return: dict
+    """
+    data = {}
+
+    # form the request
+    params = {'xml': 1}
+
+    # get the page
+    try:
+        request = requests.get(API_URL.format(user), params=params)
+        request.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+        raise SteamError("Could not get user info: {}".format(e))
+
+    profile = etree.fromstring(request.content)
+
+    try:
+        data["name"] = profile.find('steamID').text
+        data["id_64"] = int(profile.find('steamID64').text)
+        online_state = profile.find('stateMessage').text
+    except AttributeError:
+        raise SteamError("Could not get data for this user.")
+
+    online_state = online_state.replace("<br/>", ": ")  # will make this pretty later
+    data["state"] = formatting.strip_html(online_state)
+
+    data["id_32"] = convert_id32(data["id_64"])
+    data["id_3"] = convert_id3(data["id_64"])
+
+    return data
+
+
+@hook.command("steamid", "sid", "steamuser", "su")
+def steamid(text):
+    """steamid <username> -- gets the steam ID of <username>. Uses steamcommunity.com/id/<nickname>. """
+
+    try:
+        data = get_data(text)
+    except SteamError as e:
+        return "{}".format(e)
+
+    return "{name} ({state}): \x02ID64:\x02 {id_64}, \x02ID32:\x02 {id_32}, \x02ID3:\x02 {id_3}".format(**data)

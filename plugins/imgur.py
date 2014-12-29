73e590e4404014cbfef56f66d466160d31a1dfb8
Luke <lukeroge@gmail.com>
2014-12-30 09:17:28 +1300

    justify monkeypatching

--- plugins/imgur.py
+++ plugins/imgur.py
@@ -6,6 +6,10 @@ from imgurpython import ImgurClient
 from cloudbot import hook
 from cloudbot.util import web
 
+
+# imgurpython has an issue where it does not allow anonymous album creation
+# to fix this we monkeypatch logged_in to disable login checking
+# yes, it's kinda dirty, but it works :)
 ImgurClient.logged_in = lambda x: None
 
 

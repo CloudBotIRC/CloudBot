3620f1e9355685e71881783dcb616b7c6243b938
Foxlet <inquiries@comprepair.tk>
2015-01-10 18:13:05 -0500

    chatbot.py - Self timeout

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -8,12 +8,14 @@ import urllib
 def ask(text):
     """ <question> -- Asks Cleverbot <question> """
     session = cleverbot.Session()
+    timeout = 0
 
     try:
         answer = session.ask(text)
-        while answer.startswith("\n"):
+        while answer.startswith("\n") and timeout < 3:
             # cleverbot tried to advert us
             answer = session.ask(text)
+            timeout += 1
     except urllib.error.HTTPError:
         return "Could not get response. Cleverbot is angry :("
 

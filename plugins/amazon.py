14bb058b43caac5a9b5108d11e3719e5319dc43f
Luke <lukeroge@gmail.com>
2015-01-11 01:00:56 +1300

    New amazon search command! Works like newegg mostly, but has no link parser yet

---
+++ plugins/amazon.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+from cloudbot import hook
+from cloudbot.util import web, formatting
+
+
+SEARCH_URL = "http://www.amazon.com/s/"
+
+
+@hook.command("amazon", "az")
+def amazon(text):
+    """ <query> -- Searches Amazon for query"""
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.4 (KHTML, '
+                      'like Gecko) Chrome/22.0.1229.79 Safari/537.4',
+        'Referer': 'http://www.amazon.com/'
+    }
+    params = {
+        'url': 'search-alias',
+        'field-keywords': text.strip()
+    }
+    request = requests.get(SEARCH_URL, params=params, headers=headers)
+    soup = BeautifulSoup(request.text)
+
+    results = soup.find('div', {'id': 'atfResults'})
+    if not results:
+        return "No results found."
+
+    results = results.find('ul', {'id': 's-results-list-atf'}).find_all('li', {'class': 's-result-item'})
+    item = results[0]
+
+    # here we use dirty html scraping to get everything we need
+    title = formatting.truncate_str(item.find('h2', {'class': 's-access-title'}).text, 50)
+
+    try:
+        price = item.find('span', {'class': 's-price'}).text
+    except AttributeError:
+        price = item.find('span', {'class': 'a-color-price'}).text
+
+    try:
+        rating = item.find('i', {'class': 'a-icon-star'}).find('span', {'class': 'a-icon-alt'}).text
+    except AttributeError:
+        rating = "No Ratings"
+
+    url = web.try_shorten(item.find('a', {'class': 's-access-detail-page'})['href'])
+
+    return "\x02{}\x02 ({}) - {} - {}".format(title, price, rating, url)

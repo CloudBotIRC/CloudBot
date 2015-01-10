72eb3075c4ad7b0b02b5f7fa67dd94d66403d004
Luke <lukeroge@gmail.com>
2015-01-11 02:57:17 +1300

    Add rating count. Also, this command is powered by magic

--- plugins/amazon.py
+++ plugins/amazon.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
@@ -40,13 +41,16 @@ def amazon(text):
         price = item.find('span', {'class': 'a-color-price'}).text
 
     try:
+        pattern = re.compile(r'product-reviews')
         rating = item.find('i', {'class': 'a-icon-star'}).find('span', {'class': 'a-icon-alt'}).text
+        num_ratings = item.find('a', {'href': pattern}).text
+        rating_str = "{} ({} ratings)".format(rating, num_ratings)
     except AttributeError:
-        rating = "No Ratings"
+        rating_str = "No Ratings"
 
     # clean up garbage url
     o = urlparse(item.find('a', {'class': 's-access-detail-page'})['href'])
     url = o.scheme + "://" + o.netloc + o.path + "?linkCode=as2&tag=cloudbot-20"
     url = web.try_shorten(url)
 
-    return "\x02{}\x02 ({}) - {} - {}".format(title, price, rating, url)
+    return "\x02{}\x02 ({}) - {} - {}".format(title, price, rating_str, url)

a4b8ffa9512ec77994b1efa7baae37b510f7552f
Luke <lukeroge@gmail.com>
2015-01-11 13:07:47 +1300

    timeout->attempt

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -8,14 +8,14 @@ import urllib
 def ask(text):
     """ <question> -- Asks Cleverbot <question> """
     session = cleverbot.Session()
-    timeout = 0
+    attempt = 0
 
     try:
         answer = session.ask(text)
-        while answer.startswith("\n") and timeout < 3:
-            # cleverbot tried to advert us
+        while answer.startswith("\n") and attempt < 3:
+            # Cleverbot tried to advert us
             answer = session.ask(text)
-            timeout += 1
+            attempt += 1
     except urllib.error.HTTPError:
         return "Could not get response. Cleverbot is angry :("
 

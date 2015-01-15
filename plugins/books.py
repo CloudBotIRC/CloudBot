b32fc7adc48beccfc54c2bcb9e89392fe3387ac5
Luke <lukeroge@gmail.com>
2015-01-15 20:12:40 +1300

    changed message in books.py

--- plugins/books.py
+++ plugins/books.py
@@ -17,7 +17,7 @@ def load_key(bot):
 def books(text):
     """books <query> -- Searches Google Books for <query>."""
     if not dev_key:
-        return "This command requires a Google API key."
+        return "This command requires a Google Developers Console API key."
 
     json = requests.get(book_search_api, params={"q": text, "key": dev_key}).json()
 

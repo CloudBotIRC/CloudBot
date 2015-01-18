1812d5c95e1b1e3f28826d97bbbc4f5c3dbf82be
Luke <lukeroge@gmail.com>
2015-01-18 16:39:13 +1300

    Hey look at this cool new function in Python 3.4

--- plugins/whois.py
+++ plugins/whois.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 import pythonwhois
 
 from cloudbot import hook
@@ -5,30 +7,20 @@ from cloudbot import hook
 
 @hook.command
 def whois(text):
+    """<domain> -- Does a whois query on <domain>."""
     domain = text.strip().lower()
 
     data = pythonwhois.get_whois(domain, normalized=True)
-
     info = []
 
-    try:
-        i = "\x02Registrar\x02: {}".format(data["registrar"][0])
-        info.append(i)
-    except Exception as e:
-        print(e)
-        pass
+    with suppress(KeyError):
+        info.append("\x02Registrar\x02: {}".format(data["registrar"][0]))
 
-    try:
-        i = "\x02Registered\x02: {}".format(data["creation_date"][0].strftime("%d-%m-%Y"))
-        info.append(i)
-    except Exception:
-        pass
+    with suppress(KeyError):
+        info.append("\x02Registered\x02: {}".format(data["creation_date"][0].strftime("%d-%m-%Y")))
 
-    try:
-        i = "\x02Expires\x02: {}".format(data["expiration_date"][0].strftime("%d-%m-%Y"))
-        info.append(i)
-    except Exception:
-        pass
+    with suppress(KeyError):
+        info.append("\x02Expires\x02: {}".format(data["expiration_date"][0].strftime("%d-%m-%Y")))
 
     info_text = ", ".join(info)
     return "{} - {}".format(domain, info_text)

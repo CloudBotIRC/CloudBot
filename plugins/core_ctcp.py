89af7704b33461507027a66006f4391aeea59d13
Luke <lukeroge@gmail.com>
2015-01-24 16:02:44 +1300

    x_X

--- plugins/core_ctcp.py
+++ plugins/core_ctcp.py
@@ -9,7 +9,7 @@ import cloudbot
 @asyncio.coroutine
 @hook.regex(r'^\x01VERSION\x01$')
 def ctcp_version(notice):
-    notice("\x01VERSION: CloudBotRefresh v{} - http://cloudbot.pw/".format(cloudbot.__version__))
+    notice("\x01VERSION: CloudBot {} - http://cloudbot.pw/".format(cloudbot.__version__))
 
 
 @asyncio.coroutine

7f6a2703b0275169516ace1cc5007b3ac1e45cea
Nidhi Mehta <nidhididi@gmail.com>
2015-02-03 02:36:51 -0500

    Basic Commit

--- plugins/newfoods.py
+++ plugins/newfoods.py
@@ -17,6 +17,7 @@ def load_foods(bot):
     global sandwhich_data
     global mirchi_data
     global dhokla_data
+    global basic_data
 
     with codecs.open(os.path.join(bot.data_dir, "sandwich.json"), encoding="utf-8") as f:
         sandwhich_data = json.load(f)
@@ -26,6 +27,9 @@ def load_foods(bot):
         
     with codecs.open(os.path.join(bot.data_dir, "dhokla.json"), encoding="utf-8") as dData:
         dhokla_data = json.load(dData)
+
+    with codecs.open(os.path.join(bot.data_dir, "basic.json"), encoding="utf-8") as bData:
+        basic_data = json.load(bData)
 
 def is_self(conn, target):
     """
@@ -96,6 +100,30 @@ def sandwich(text, conn, nick, notice, action):
 
     generator = textgen.TextGenerator(sandwhich_data["templates"], sandwhich_data["parts"],
                                       variables={"user": target})
+
+    # act out the message
+    action(generator.generate_string())
+
+@asyncio.coroutine
+@hook.command()
+def basic(text, conn, nick, notice, action):
+    """<user> - show how basic you are to <user>
+    :type text: str
+    :type conn: cloudbot.client.Client
+    :type nick: str
+    """
+    target = text.strip()
+
+    if " " in target:
+        notice("Invalid username!")
+        return
+
+    # if the user is trying to make the bot kill itself, kill them
+    if is_self(conn, target):
+        target = nick
+    
+    generator = textgen.TextGenerator(basic_data["templates"], basic_data["parts"],
+                                        variables={"user": target})
 
     # act out the message
     action(generator.generate_string())

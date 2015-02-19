f6613aa749fbe076873bf34e33d56af4713f8581
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-02-18 20:14:11 -0500

    ported cats.py over to requests + PEP8

--- plugins/cats.py
+++ plugins/cats.py
@@ -1,20 +1,22 @@
+import requests
 from cloudbot import hook
-from cloudbot.util import http, web
 
 
 @hook.command(autohelp=False)
 def cats():
     """gets a fucking fact about cats."""
-    
+
     attempts = 0
     while True:
         try:
-            json = http.get_json('http://catfacts-api.appspot.com/api/facts?number=1')
+            r = requests.get(
+                'http://catfacts-api.appspot.com/api/facts?number=1')
         except:
             if attempts > 2:
                 return "There was an error contacting the API."
             else:
                 attempts += 1
                 continue
+        json = r.json()
         response = json['facts']
-        return response 
+        return response

1384bce8234b5bc020ffc9838415d11937c5ce98
Luke <lukeroge@gmail.com>
2015-02-20 18:27:10 +1300

    Improve wyr formatting

--- plugins/wyr.py
+++ plugins/wyr.py
@@ -2,43 +2,43 @@ import requests
 
 from cloudbot import hook
 
-headers = {'User-Agent': 'CloudBot/dev 1.0 - CloudBot Refresh by lukeroge'}
+API_URL = "http://www.rrrather.com/botapi"
 
-@hook.command
-def wyr(text, message):
-    url = "http://www.rrrather.com/botapi"
 
-    r = requests.get(url, headers=headers)
+@hook.command("wyr", "wouldyourather")
+def wyr(bot):
+    headers = {"User-Agent": bot.user_agent}
+
+    r = requests.get(url=API_URL, headers=headers)
     data = r.json()
-    title = data['title'].strip().capitalize()
-    title = title.rstrip('.?')
-    choice1 = data['choicea'].strip().lower()
-    choice1 = choice1.rstrip('.?')
-    choice2 = data['choiceb'].strip().lower()
-    choice2 = choice2.rstrip('.?')
+
+    # decrapify text
+    title = data['title'].strip().capitalize().rstrip('.?,')
+    choice1 = data['choicea'].strip().lower().rstrip('.?,!').lstrip('.')
+    choice2 = data['choiceb'].strip().lower().rstrip('.?,!').lstrip('.')
     link = data['link']
 
-    text = "{} {}".format(choice1, choice2).split()
-    text = [ word for word in text if word != "a" ]
+    # get all the words in the answers
+    text = choice1.split() + choice2.split()
+    text = [word for word in text if word != "a"]
 
     title_text = title.split()
 
-    #message(str(title_text))
-    #message(str(text))
+    print(str(title_text))
+    print(str(text))
 
     dupl_count = 0
 
     for word in title_text:
         dupl_count += text.count(word)
 
-    #message(str(dupl_count / len(text)))
+    print(str(dupl_count / len(text)))
 
-    if title.lower() == "would u rather":
-        title = "Would you rather"
+    title = title.replace(" u ", " you ")
 
+    # detect if the answers are also in the question
+    # if so, replace question with a generic one
     if dupl_count / len(text) >= 0.6:
         title = "Would you rather"
-    else:
-        pass
 
     return "{}... {} \x02OR\x02 {}? - {}".format(title, choice1, choice2, link)

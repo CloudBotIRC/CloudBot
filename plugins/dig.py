83a03c7a788e7d194df1beb82bea27dd414b543a
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-02-23 17:53:56 -0500

    pep8 for dig.py

--- plugins/dig.py
+++ plugins/dig.py
@@ -4,8 +4,6 @@ import json
 from cloudbot import hook
 
 
-base_url = "http://dig.jsondns.org/IN/"
-
 @hook.command
 def dig(text):
     """.dig <domain> <recordtype> returns a list of records for the specified domain valid record types are A, NS, and MX. If a record type is not chosen A will be the default."""
@@ -21,13 +19,14 @@ def dig(text):
     r = requests.get(url)
     results = r.json()
     out = "The following records were found for {}: ".format(domain)
-    if results['header']['rcode']== "NXDOMAIN":
+    if results['header']['rcode'] == "NXDOMAIN":
         return "no dns record for {} was found".format(domain)
     for r in range(len(results['answer'])):
         domain = results['answer'][r]['name']
         rtype = results['answer'][r]['type']
         ttl = results['answer'][r]['ttl']
         rdata = results['answer'][r]['rdata']
-        out += "name: {} type: {} ttl: {} rdata: {} | ".format(domain, rtype, ttl, rdata)
+        out += "name: {} type: {} ttl: {} rdata: {} | ".format(
+            domain, rtype, ttl, rdata)
     out = out[:-2]
     return out

cb04ad8d79d6202c1c19557ce55eafd889cb1285
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-02-23 21:51:06 -0500

    change some formattingand add txt records.

--- plugins/dig.py
+++ plugins/dig.py
@@ -6,11 +6,11 @@ from cloudbot import hook
 
 @hook.command
 def dig(text):
-    """.dig <domain> <recordtype> returns a list of records for the specified domain valid record types are A, NS, and MX. If a record type is not chosen A will be the default."""
+    """.dig <domain> <recordtype> returns a list of records for the specified domain valid record types are A, NS, TXT, and MX. If a record type is not chosen A will be the default."""
     try:
         domain, rtype = text.split()
         rtype = rtype.upper()
-        if rtype not in ["A", "NS", "MX"]:
+        if rtype not in ["A", "NS", "MX", "TXT"]:
             rtype = "A"
     except:
         domain = text.strip()
@@ -19,14 +19,18 @@ def dig(text):
     r = requests.get(url)
     results = r.json()
     out = "The following records were found for {}: ".format(domain)
-    if results['header']['rcode'] == "NXDOMAIN":
+    if results['header']['rcode'] == "NXDOMAIN" or not results['header']['answer']:
         return "no dns record for {} was found".format(domain)
+    out = "The following records were found for {}: ".format(domain)
     for r in range(len(results['answer'])):
         domain = results['answer'][r]['name']
         rtype = results['answer'][r]['type']
         ttl = results['answer'][r]['ttl']
-        rdata = results['answer'][r]['rdata']
-        out += "name: {} type: {} ttl: {} rdata: {} | ".format(
+        if rtype == "MX":
+            rdata = results['answer'][r]['rdata'][1]
+        else:
+            rdata = results['answer'][r]['rdata']
+        out += "name: \x02{}\x02 type: \x02{}\x02 ttl: \x02{}\x02 rdata: \x02{}\x02 | ".format(
             domain, rtype, ttl, rdata)
     out = out[:-2]
     return out

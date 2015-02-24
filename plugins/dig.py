0fbcb1bb83708de866f281da3ef8bb4782aa90ae
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-02-23 22:06:18 -0500

    formatting. switch to iter notices for each record.

--- plugins/dig.py
+++ plugins/dig.py
@@ -21,16 +21,16 @@ def dig(text, nick, notice):
     out = "The following records were found for {}: ".format(domain)
     if results['header']['rcode'] == "NXDOMAIN":
         return "no dns record for {} was found".format(domain)
-    out = "The following records were found for {}: ".format(domain)
+    out = "The following records were found for \x02{}\x02: ".format(domain)
     for r in range(len(results['answer'])):
         domain = results['answer'][r]['name']
         rtype = results['answer'][r]['type']
         ttl = results['answer'][r]['ttl']
         if rtype == "MX":
             rdata = results['answer'][r]['rdata'][1]
+        elif rtype == "TXT":
+            rdata = results['answer'][r]['rdata'][0]
         else:
             rdata = results['answer'][r]['rdata']
-        out += "name: \x02{}\x02 type: \x02{}\x02 ttl: \x02{}\x02 rdata: \x02{}\x02 | ".format(
-            domain, rtype, ttl, rdata)
-    out = out[:-2]
-    notice(out, nick)
+        notice( "name: \x02{}\x02 type: \x02{}\x02 ttl: \x02{}\x02 rdata: \x02{}\x02".format(
+            domain, rtype, ttl, rdata), nick)

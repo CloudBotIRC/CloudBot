dd1ff1b9d9f16061aad5ad26c10757aa530a6700
Luke <lukeroge@gmail.com>
2015-03-14 20:29:11 +1300

    Correction now uses a case-insensitive replace function that tries to keep casing the same. Closes #109

--- plugins/correction.py
+++ plugins/correction.py
@@ -6,7 +6,31 @@ from cloudbot import hook
 correction_re = re.compile(r"^[sS]/(.*/.*(?:/[igx]{,4})?)\S*$")
 
 
-@asyncio.coroutine
+def i_replace(text, old, new, count=None):
+    idx = 0
+    num = 0
+    while idx < len(text):
+        index_l = text.lower().find(old.lower(), idx)
+        if index_l == -1:
+            return text
+
+        to_replace = text[index_l:index_l + len(old)]
+        if to_replace.isupper():
+            text = text[:index_l] + "\x02" + new.upper() + "\x02" + text[index_l + len(old):]
+        elif to_replace.islower():
+            text = text[:index_l] + "\x02" + new.lower() + "\x02" + text[index_l + len(old):]
+        elif to_replace[0].isupper():
+            text = text[:index_l] + "\x02" + new.capitalize() + "\x02" + text[index_l + len(old):]
+        else:
+            text = text[:index_l] + "\x02" + new + "\x02" + text[index_l + len(old):]
+
+        idx = index_l + len(old)
+        num += 1
+        if count and num >= count:
+            break
+    return text
+
+
 @hook.regex(correction_re)
 def correction(match, conn, chan, message):
     """
@@ -16,29 +40,25 @@ def correction(match, conn, chan, message):
     """
     groups = [b.replace("\/", "/") for b in re.split(r"(?<!\\)/", match.groups()[0])]
     find = groups[0]
-    replacement = groups[1]
-    flags = groups[2] if len(groups) == 3 else ""
-    find_re = find
-    #find_re = re.compile("{}{}".format("(?{})".format(flags.replace("g", ""))
-    #                                   if flags.replace("g", "") != "" else "", find))
+    replace = groups[1]
 
     for item in conn.history[chan].__reversed__():
         nick, timestamp, msg = item
         if correction_re.match(msg):
             # don't correct corrections, it gets really confusing
             continue
-        #if find_re.search(msg):
-        if find_re in msg:
-            is_action = False
+
+        if find.lower() in msg.lower():
             if "\x01ACTION" in msg:
-                is_action = True
                 msg = msg.replace("\x01ACTION", "").replace("\x01", "")
-            #mod_msg = find_re.sub("\x02" + replacement + "\x02", msg, count=int("g" not in flags))
-            mod_msg = msg.replace(find_re, "\x02" + replacement + "\x02")
-            message("Correction, {}".format(("<{}> " if not is_action else "* {}").format(nick) + mod_msg))
-            # append to end of history file
-            #msg = find_re.sub(replacement, msg, count=int("g" not in flags))
-            msg = msg.replace(find_re, replacement)
+                mod_msg = i_replace(msg, find, replace)
+                message("Correction, * {} {}".format(nick, mod_msg))
+            else:
+                mod_msg = i_replace(msg, find, replace)
+                message("Correction, <{}> {}".format(nick, mod_msg))
+
+            # append to end of history
+            msg = i_replace(msg, find, replace)
             conn.history[chan].append((nick, timestamp, msg))
             return
         else:

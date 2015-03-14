28640daaee859b7cc1f6918d83eb1c813bde99c6
Luke <lukeroge@gmail.com>
2015-03-14 21:48:46 +1300

    Move ireplace to util/formatting, get rid of janky capitalisation code

--- plugins/correction.py
+++ plugins/correction.py
@@ -1,41 +1,10 @@
-import asyncio
 import re
 
 from cloudbot import hook
 
+from cloudbot.util.formatting import ireplace
+
 correction_re = re.compile(r"^[sS]/(.*/.*(?:/[igx]{,4})?)\S*$")
-
-
-def ireplace(text, old, new, count=None):
-    """
-    A case-insensitive replace() clone. Return a copy of text with all occurrences of substring
-    old replaced by new. If the optional argument count is given, only the first count
-    occurrences are replaced.
-    This function also checks the casing of the words it is replacing, and attempts to maintain
-    the same casing the word had before.
-    """
-    idx = 0
-    num = 0
-    while idx < len(text):
-        index_l = text.lower().find(old.lower(), idx)
-        if index_l == -1:
-            return text
-
-        to_replace = text[index_l:index_l + len(old)]
-        if to_replace.isupper():
-            text = text[:index_l] + "\x02" + new.upper() + "\x02" + text[index_l + len(old):]
-        elif to_replace.islower():
-            text = text[:index_l] + "\x02" + new.lower() + "\x02" + text[index_l + len(old):]
-        elif to_replace[0].isupper():
-            text = text[:index_l] + "\x02" + new.capitalize() + "\x02" + text[index_l + len(old):]
-        else:
-            text = text[:index_l] + "\x02" + new + "\x02" + text[index_l + len(old):]
-
-        idx = index_l + len(old)
-        num += 1
-        if count and num >= count:
-            break
-    return text
 
 
 @hook.regex(correction_re)
@@ -58,13 +27,12 @@ def correction(match, conn, chan, message):
         if find.lower() in msg.lower():
             if "\x01ACTION" in msg:
                 msg = msg.replace("\x01ACTION", "").replace("\x01", "")
-                mod_msg = ireplace(msg, find, replace)
+                mod_msg = ireplace(msg, find, "\x02" + replace + "\x02")
                 message("Correction, * {} {}".format(nick, mod_msg))
             else:
-                mod_msg = ireplace(msg, find, replace)
+                mod_msg = ireplace(msg, find, "\x02" + replace + "\x02")
                 message("Correction, <{}> {}".format(nick, mod_msg))
 
-            # append to end of history
             msg = ireplace(msg, find, replace)
             conn.history[chan].append((nick, timestamp, msg))
             return

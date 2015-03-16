a9f6748d2416ceb054a92db3179cd736074fa814
Foxlet <inquiries@comprepair.tk>
2015-03-15 22:44:59 -0400

    Requests! - google.py

--- plugins/google.py
+++ plugins/google.py
@@ -1,12 +1,14 @@
 # RoboCop 2's gsearch.py - A replacement for google.py after Google's deprecation of Google Web Search API
 # Module requires a Google Custom Search API key and a Custom Search Engine ID in order to function.
 
-import urllib.request, urllib.parse
+import urllib.parse
+import requests
 
 from cloudbot import hook
 from cloudbot.util import http, formatting
 
 API_CS = 'https://www.googleapis.com/customsearch/v1?cx={}&q={}&key={}'
+
 
 @hook.on_start()
 def load_api(bot):
@@ -16,22 +18,16 @@ def load_api(bot):
     dev_key = bot.config.get("api_keys", {}).get("google_dev_key", None)
     cx = bot.config.get("api_keys", {}).get("google_cse_id", None)
 
-def api_get(query, bot):
-    """Use the RESTful Google Search API"""
-    # YOU NEED A KEY TO USE THIS MODULE!!!
-    # [key] is your Google Developers Project API Key, and [cx] is the custom search engine ID to use for requests.
-
-    url = API_CS.format(urllib.parse.quote(cx), query, urllib.parse.quote(dev_key))
-    return http.get_json(url)
 
 @hook.command('g','gse')
 def gse(text, bot):
     """google <query> -- Returns first google search result for <query>."""
     if not dev_key:
         return "This command requires a Google Developers Console API key."
+    if not cx:
+        return "This command requires a custom Google Search Engine ID."
 
-    eval = urllib.parse.quote(text)
-    parsed = api_get(eval, bot)
+    parsed = requests.get(API_CS.format(urllib.parse.quote(cx), urllib.parse.quote(text), urllib.parse.quote(dev_key)))
 
     try:
         result = parsed['items'][0]

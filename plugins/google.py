4ed9d64779869646ab4ca021c7b6c20d6d89dfeb
Foxlet <inquiries@comprepair.tk>
2015-03-15 22:26:08 -0400

    Fast Foward! - google.py

--- plugins/google.py
+++ plugins/google.py
@@ -1,48 +1,41 @@
-# RoboCop 2's google.py - A backport of the Google module from classic RoboCop, using urllib instead of requests
+# RoboCop 2's gsearch.py - A replacement for google.py after Google's deprecation of Google Web Search API
+# Module requires a Google Custom Search API key and a Custom Search Engine ID in order to function.
 
 import random
+
+import urllib.request, urllib.parse
 
 from cloudbot import hook
 from cloudbot.util import http, formatting
 
-def api_get(kind, query):
+
+def api_get(query, bot):
     """Use the RESTful Google Search API"""
-    url = 'https://ajax.googleapis.com/ajax/services/search/%s?' \
-          'v=1.0&safe=moderate'
-    return http.get_json(url % kind, q=query)
+    # YOU NEED A KEY TO USE THIS MODULE!!!
+    # [key] is your Google Developers Project API Key, and [cx] is the custom search engine ID to use for requests.
+    key = urllib.parse.quote(bot.config.get("api_keys", {}).get("google_dev_key"))
+    cx = urllib.parse.quote(bot.config.get("api_keys", {}).get("google_cse_id"))
 
+    url = 'https://www.googleapis.com/customsearch/v1?cx=' + cx + '&q='+ query + '&key=' + key
+    return http.get_json(url)
 
-@hook.command('image')
-@hook.command('gis')
+@hook.command('g')
+@hook.command('gse')
 @hook.command
-def googleimage(text):
-    """gis <query> -- Returns first Google Image result for <query>."""
+def gse(text, bot):
+    """gsearch <query> -- Returns first google search result for <query>."""
 
-    parsed = api_get('images', text)
-    if not 200 <= parsed['responseStatus'] < 300:
-        raise IOError('error searching for images: {}: {}'.format(parsed['responseStatus'], ''))
-    if not parsed['responseData']['results']:
-        return 'no images found'
-    return random.choice(parsed['responseData']['results'][:10])['unescapedUrl']
+    eval = urllib.parse.quote(text)
+    parsed = api_get(eval, bot)
 
+    try:
+        result = parsed['items'][0]
+    except KeyError:
+        return "No results found."
 
-@hook.command('search')
-@hook.command('oldg')
-@hook.command
-def google(text):
-    """google <query> -- Returns first google search result for <query>."""
-
-    parsed = api_get('web', text)
-    if not 200 <= parsed['responseStatus'] < 300:
-        raise IOError('error searching for pages: {}: {}'.format(parsed['responseStatus'], ''))
-    if not parsed['responseData']['results']:
-        return 'No results found.'
-
-    result = parsed['responseData']['results'][0]
-
-    title = http.unescape(result['titleNoFormatting'])
+    title = http.unescape(result['title'])
     title = formatting.truncate_str(title, 60)
-    content = http.unescape(result['content'])
+    content = http.unescape(result['snippet'])
 
     if not content:
         content = "No description available."
@@ -50,4 +43,4 @@ def google(text):
         content = http.html.fromstring(content).text_content()
         content = formatting.truncate_str(content, 150)
 
-    return u'{} -- \x02{}\x02: "{}"'.format(result['unescapedUrl'], title, content)
+    return u'{} -- \x02{}\x02: "{}"'.format(result['link'], title, content)

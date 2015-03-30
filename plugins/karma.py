af0f89f021dfc5573bf59591520d37986aca8ff0
Luke <lukeroge@gmail.com>
2015-03-30 19:14:53 +1300

    Tweak a few things in karma

--- plugins/karma.py
+++ plugins/karma.py
@@ -1,13 +1,16 @@
-from cloudbot import hook
-from cloudbot.util import timeformat, formatting, database
-
 import time
 import re
 
-CAN_DOWNVOTE = False
-
 from sqlalchemy import Table, Column, Integer, String, PrimaryKeyConstraint
 from sqlalchemy import select
+
+from cloudbot import hook
+from cloudbot.util import timeformat, formatting, database
+
+
+CAN_DOWNVOTE = False
+TIME_LIMIT = 300.0
+
 
 karma_table = Table(
     'karma',
@@ -15,6 +18,7 @@ karma_table = Table(
     Column('nick_vote', String(25)),
     Column('up_karma', Integer, default=0),
     Column('down_karma', Integer, default=0),
+    Column('total_karma', Integer, default=0),  # used for querying
     PrimaryKeyConstraint('nick_vote')
 )
 
@@ -29,7 +33,8 @@ def up(db, nick_vote):
         down_karma,
         total_karma) values(:nick,0,0,0)""", {'nick': nick_vote.lower()})
     query = karma_table.update().values(
-        up_karma=karma_table.c.up_karma + 1
+        up_karma=karma_table.c.up_karma + 1,
+        total_karma=karma_table.c.total_karma + 1
     ).where(karma_table.c.nick_vote == nick_vote.lower())
     db.execute(query)
     db.commit()
@@ -43,7 +48,8 @@ def down(db, nick_vote):
         down_karma,
         total_karma) values(:nick,0,0,0)""", {'nick': nick_vote.lower()})
     query = karma_table.update().values(
-        down_karma=karma_table.c.down_karma + 1
+        down_karma=karma_table.c.down_karma + 1,
+        total_karma=karma_table.c.total_karma - 1
     ).where(karma_table.c.nick_vote == nick_vote.lower())
     db.execute(query)
     db.commit()
@@ -52,19 +58,19 @@ def down(db, nick_vote):
 def allowed(uid):
     """ checks if a user is allowed to vote, and keeps track of voters """
     global voters
-    time_restriction = 300.0
 
     # clear expired voters
     for _uid, _timestamp in voters.items():
-        if (time.time() - _timestamp) >= time_restriction:
+        if (time.time() - _timestamp) >= TIME_LIMIT:
             del voters[_uid]
 
     if uid in voters:
         last_voted = voters[uid]
-        return False, timeformat.time_until(last_voted, now=time.time() - time_restriction)
+        return False, timeformat.time_until(last_voted, now=time.time() - TIME_LIMIT)
     else:
         voters[uid] = time.time()
         return True, 0
+
 
 karma_re = re.compile('^([a-z0-9_\-\[\]\\^{}|`]{3,})(\+\+|\-\-)$', re.I)
 
@@ -93,22 +99,30 @@ def karma_add(match, nick, conn, db, notice):
 
 
 @hook.command('karma', 'k')
-def karma(text, chan, db):
+def karma(text, db):
     """k/karma <nick> -- returns karma stats for <nick>"""
-
-    if not chan.startswith('#'):
-        return
-
-    nick_vote = text
-
     query = db.execute(
         select([karma_table])
-        .where(karma_table.c.nick_vote == nick_vote.lower())
+        .where(karma_table.c.nick_vote == text.lower())
+    ).fetchone()
+
+    if not query:
+        return "That user has no karma :("
+    else:
+        karma_text = formatting.pluralize(query['up_karma'] - query['down_karma'], '\x02karma')
+        return "{} has \x02{}\x02 karma!".format(text, query['up_karma'] - query['down_karma'])
+
+
+@hook.command('loved')
+def loved(text, db):
+    """loved -- Shows the users with the most karma!"""
+    query = db.execute(
+        select([karma_table])
+        .order_by(karma_table.c.total_karma.desc())
+        .limit(5)
     ).fetchall()
 
     if not query:
-        return "That user has no karma."
+        return "??"
     else:
-        query = query[0]
-        karma_text = formatting.pluralize(query['up_karma'] - query['down_karma'], 'karma point')
-        return "{} has {}.".format(nick_vote, karma_text)
+        return query

d83c59b65988728408760ef406f43d44ae02aef4
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-04-05 09:30:34 -0400

    grab rewrite almost complete. use cache json variable for reads, minimize db queries for writes

--- plugins/grab.py
+++ plugins/grab.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 from sqlalchemy import Table, Column, String, Boolean, DateTime
 from sqlalchemy.sql import select
@@ -35,7 +36,7 @@ def load_cache(db):
     """
     global grab_cache
     grab_cache = {}
-    for row in db.execute(table.select()):
+    for row in db.execute(table.select().order_by(table.c.time)):
         name = row["name"]
         quote = row["quote"]
         chan = row["chan"]
@@ -57,11 +58,7 @@ def check_grabs(name, quote, chan):
 
 def grab_add(nick, time, msg, chan, db, conn):
     # Adds a quote to the grab table
-    # add the nick, chan, timestamp, msg to the database
-    db.execute(
-        "insert or replace into grab(name, time, quote, chan)"
-        " values(:name, :time, :quote, :chan)",
-        {'name': nick, 'time': time, 'quote': msg, 'chan': chan})
+    db.execute(table.insert().values(name=nick, time=time, quote=msg, chan=chan))
     db.commit()
     load_cache(db)
 
@@ -72,10 +69,6 @@ def grab(text, nick, chan, db, conn):
     specified nick and adds it to the quote database"""
     if text.lower() == nick.lower():
         return "Didn't your mother teach you not to grab yourself?"
-    db_init(db, conn.name)
-    #lastq = db.execute("select name, time, quote, chan from seen_user "
-    #                   "where name = :name and chan = :chan ",
-    #                   {'name': text.lower(), 'chan': chan}).fetchone()
     
     for item in conn.history[chan].__reversed__():
         name, timestamp, msg = item
@@ -90,40 +83,7 @@ def grab(text, nick, chan, db, conn):
                 if check_grabs(name.lower(), msg, chan):
                     return "the operation succeeded."
                 break
-        return "I couldn't find anything from {} in recent history.".format(text)
-    if lastq:
-        #msg = ""
-        #if lastq[2].startswith("\x01ACTION"):
-        #    msg = lastq[2].replace("\x01ACTION", "* ").replace("\x01", "")
-        #else:
-        #    msg = lastq[2]
-
-        check = db.execute("select * from grab where name = :name "
-                           "and quote = :quote and chan = :chan ",
-                           {'name': lastq[0], 'quote': lastq[2],
-                            'chan': lastq[3]}).fetchone()
-        if check:
-            return "the quote has already been added to the database."
-        else:
-            #if lastq[2].startswith("\x01ACTION"):
-            #    msg = lastq[2].replace("\x01ACTION", "* ").replace("\x01", "")
-            #else:
-            #    msg = lastq[2]    
-            grab_add(lastq[0], lastq[1], lastq[2], lastq[3], db, conn)
-            lastcheck = db.execute("select * from grab "
-                                   "where name = :name and quote = :quote and "
-                                   "chan = :chan ", {'name': lastq[0],
-                                                     'quote': lastq[2],
-                                                     'chan': lastq[3]}
-                                   ).fetchone()
-            if lastcheck:
-                return ("the operation succeeded.")
-            else:
-                return ("There was a problem adding {} to the DB"
-                        ).format(lastq)
-    else:
-        return ("I couldn't find anything from {}"
-                " in the recent messages.".format(text))
+    return "I couldn't find anything from {} in recent history.".format(text)
 
 def format_grab(name, quote):
     if quote.startswith("\x01ACTION") or quote.startswith("*"):
@@ -137,50 +97,56 @@ def format_grab(name, quote):
 @hook.command()
 def lastgrab(text, chan, db, message, conn):
     """prints the last grabbed quote from <nick>."""
-    db_init(db, conn.name)
-    lgrab = db.execute("select name, quote from grab "
-                       "where name = :name and chan = :chan order by time "
-                       "desc", {'name': text.lower(), 'chan': chan}).fetchone()
-    if lgrab:
-        quote = lgrab[1]
-        message(format_grab(text, quote),chan)
-    else:
+    lgrab = ""
+    try:
+        lgrab = grab_cache[chan][text.lower()][-1]
+    except:
         return "<{}> has never been grabbed.".format(text)
+    if lgrab:
+        quote = lgrab
+        message(format_grab(text, quote),chan)
 
 
 @hook.command(autohelp=False)
 def grabrandom(text, chan, message, db, conn):
     """grabs a random quote from the grab database"""
-    db_init(db, conn.name)
-    grablist = ""
+    grab = ""
     name = ""
+    pick_name = random.choice(list(grab_cache[chan].keys()))
     if not text:
-        grablist = db.execute("select name, quote from grab "
-                              "where chan = :chan order by random()",
-                              {'chan': chan}).fetchone()
-        name = grablist[0]
+        try:
+            grab = random.choice(grab_cache[chan][pick_name])
+            name = pick_name
+        except:
+            return "There was either a major fuck up or no quotes have been grabbed in this channel."
     else:
         name = text.split(' ')[0]
-        grablist = db.execute("select name, quote from grab "
-                              "where chan = :chan and name = :name "
-                              "order by random()",
-                              {'chan': chan, 'name': name.lower()}).fetchone()
-    if grablist:
-        quote = grablist[1]
-        message(format_grab(name, quote), chan)
+        try:
+            grab = random.choice(grab_cache[chan][name])
+        except:
+            return "it appears {} has never been grabbed"
+    if grab:
+        message(format_grab(name, grab), chan)
     else:
-        return ("somebody really fucked up, or no quotes have been "
-                "grabbed from that nick.")
+        return "Hmmm try grabbing a quote first."
 
 
 @hook.command(autohelp=False)
 def grabsearch(text, chan, db, conn):
     """.grabsearch <text> matches "text" against nicks or grab strings in the database"""
     out = ""
-    result = db.execute(
-        "select name, quote from grab where (chan = :chan) and (name like :name or quote like :quote)", {
-            "chan": chan, "name": "%{}%".format(
-                text.lower()), "quote": "%{}%".format(text)}).fetchall()
+    result = []
+    try:
+        quotes = grab_cache[chan][text.lower()]
+        for grab in quotes:
+            result.append((text, grab))
+    except:
+       pass
+    for name in grab_cache[chan]:
+        for grab in grab_cache[chan][name]:
+            if name != text.lower():
+                if text.lower() in grab.lower():
+                    result.append((name, grab))
     if result:
         for grab in result:
             name = grab[0]

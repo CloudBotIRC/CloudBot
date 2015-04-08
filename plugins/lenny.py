bd6729b5b4f4430fa4286d2e1359c6e8fe25ac42
archaicmotion <william.dungan@gmail.com>
2015-04-08 11:41:28 -0400

    LEARNING CHANGE

--- plugins/lenny.py
+++ plugins/lenny.py
@@ -1,6 +1,6 @@
 from cloudbot import hook
 
 @hook.command(autohelp=False)
-def lenny(text, message):
+def lenny(message):
     """why the shit not lennyface"""
     message(u'(\u0361\xb0 \u035c\u0296 \u0361\xb0)')

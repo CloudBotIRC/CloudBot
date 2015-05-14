fe9e6e517f928c07bb30ce00a823080c420ce232
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-05-14 17:13:04 -0400

    fix metars.py

--- plugins/metars.py
+++ plugins/metars.py
@@ -9,13 +9,13 @@ def get_key(bot):
     """Gets the username to use in the geonames request"""
     global api_user
     api_user = bot.config.get("api_keys", {}).get("geonames")
-    if not api_user:
-        return "no api_user has been specified."
 
 
 @hook.command()
 def metar(text):
     """metars [ICAO station code] returns the metars information for the specified station. A list of station codes can be found here: http://weather.rap.ucar.edu/surface/stations.txt"""
+    if not api_user:
+        return "no api_user has been specified."
     station = text.split(' ')[0].upper()
     if not len(station) == 4:
         return "please specify a valid station code see http://weather.rap.ucar.edu/surface/stations.txt for a list."

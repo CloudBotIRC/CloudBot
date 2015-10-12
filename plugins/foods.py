6c0513e89714a43195c8f71cb091ad37e6ba8216
Sprinting Dev <levelheadedidiot@gmail.com>
2015-10-12 17:41:15 -0400

    Added ice-cream command.

--- plugins/foods.py
+++ plugins/foods.py
@@ -66,7 +66,7 @@ def load_foods(bot):
     :type bot: cloudbot.bot.CloudBot
     """
     global sandwich_data, taco_data, coffee_data, noodles_data, muffin_data, \
-        tea_data, keto_data, beer_data, cheese_data, pancake_data, chicken_data
+        tea_data, keto_data, beer_data, cheese_data, pancake_data, chicken_data, icecream_data
 
     with codecs.open(os.path.join(bot.data_dir, "sandwich.json"), encoding="utf-8") as f:
         sandwich_data = json.load(f)
@@ -100,6 +100,9 @@ def load_foods(bot):
 
     with codecs.open(os.path.join(bot.data_dir, "chicken.json"), encoding="utf-8") as f:
         chicken_data = json.load(f)
+
+    with codecs.open(os.path.join(bot.data_dir, "icecream.json"), encoding="utf-8") as f:
+        icecream_data = json.load(f)
 
 		
 @asyncio.coroutine
@@ -316,3 +319,18 @@ def chicken(text, action):
 
     # act out the message
     action(generator.generate_string())
+
+@asyncio.coroutine
+@hook.command
+def icecream(text, action):
+    """<user> - give icecream to <user>"""
+    user = text.strip()
+
+    if not is_valid(user):
+        return "I can't give icecream to that user."
+
+    generator = textgen.TextGenerator(icecream_data["templates"], icecream_data["parts"], variables={"user": user})
+
+    # act out the message
+    action(generator.generate_string())
+

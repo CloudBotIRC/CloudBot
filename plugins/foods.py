dbc33cac6c549c4e6c84028496d6b5619b00bc47
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-10-23 18:12:44 -0400

    add doobie def, and define doobie_data as a global variable

--- plugins/foods.py
+++ plugins/foods.py
@@ -66,7 +66,8 @@ def load_foods(bot):
     :type bot: cloudbot.bot.CloudBot
     """
     global sandwich_data, taco_data, coffee_data, noodles_data, muffin_data, \
-        tea_data, keto_data, beer_data, cheese_data, pancake_data, chicken_data, icecream_data, brekkie_data
+        tea_data, keto_data, beer_data, cheese_data, pancake_data, chicken_data, \
+        icecream_data, brekkie_data, doobie_data
 
     with codecs.open(os.path.join(bot.data_dir, "sandwich.json"), encoding="utf-8") as f:
         sandwich_data = json.load(f)
@@ -356,6 +357,7 @@ def brekkie(text, action):
 
 @asyncio.coroutine
 @hook.command("doobie")
+def doobie(text, action):
     """<user> - pass the doobie to <user>"""
     user = text.strip()
 

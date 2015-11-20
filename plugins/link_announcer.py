f64b4279c9ef25720c2a92b2a6b5616343d583c4
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-11-20 17:44:05 -0500

    add xkcd to the blacklist regex of link_announcer.py

--- plugins/link_announcer.py
+++ plugins/link_announcer.py
@@ -6,7 +6,7 @@ from cloudbot import hook
 
 # This will match ANY we url including youtube, reddit, twitch, etc... Some additional work needs to go into
 # not sending the web request etc if the match also matches an existing web regex.
-blacklist = re.compile('.*(reddit\.com|redd.it|youtube.com|youtu.be|spotify.com|twitter.com|twitch.tv|amazon.co|amzn.com|steamcommunity.com|steampowered.com|newegg.com).*', re.I)
+blacklist = re.compile('.*(reddit\.com|redd.it|youtube.com|youtu.be|spotify.com|twitter.com|twitch.tv|amazon.co|xkcd.com|amzn.com|steamcommunity.com|steampowered.com|newegg.com).*', re.I)
 url_re = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
 opt_in = []

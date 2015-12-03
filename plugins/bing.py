0e1ce755dde7e9f637e8fcc890c9cc4ab0ba6dec
Andrew Edwards <andrewthomasedwards@gmail.com>
2015-12-02 22:24:51 -0500

    switch to bing for image searches for a minute.

--- plugins/bing.py
+++ plugins/bing.py
@@ -71,7 +71,7 @@ def bing(text, bot):
     return colors.parse('{} -- $(b){}$(b): "{}"'.format(url, title, desc))
 
 
-@hook.command("bingimage", "bis")
+@hook.command("bingimage", "bis", "gis", "image")
 def bingimage(text, bot):
     """<query> - returns the first bing image search result for <query>"""
     api_key = bot.config.get("api_keys", {}).get("bing_azure")

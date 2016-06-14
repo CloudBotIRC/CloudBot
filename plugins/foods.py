4813f30ce10e49a2897504e45a8a3ad92299cf1b
starsparrow <starsparrow@users.noreply.github.com>
2016-06-14 14:24:34 -0400

    Update foods.py
    Added data sources and command
    Conflicts:
    	plugins/foods.py

--- plugins/foods.py
+++ plugins/foods.py
@@ -69,7 +69,11 @@ def load_foods(bot):
         tea_data, keto_data, beer_data, cheese_data, pancake_data, chicken_data, \
         icecream_data, brekkie_data, doobie_data, pizza_data, chocolate_data, pasta_data, \
 <<<<<<< HEAD
+<<<<<<< HEAD
         nugget_data, cereal_data
+=======
+        nugget_data, pie_data
+>>>>>>> be7d3ce... Update foods.py
 =======
         nugget_data, pie_data
 >>>>>>> be7d3ce... Update foods.py

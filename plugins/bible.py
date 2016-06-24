467491a337b438323706c1b48debd969c0523e06
Andrew Edwards <andrewthomasedwards@gmail.com>
2016-06-24 11:58:44 -0400

    use json return type and modify some formatting in bible.py

--- plugins/bible.py
+++ plugins/bible.py
@@ -3,17 +3,22 @@ import re
 
 from cloudbot import hook
 
-remove_html = re.compile("<.*?>")
-
 @hook.command("bible", "passage", singlethreaded=True)
 def bible(text):
     """Prints the specified passage from the Bible"""
     passage = text.strip()
-    r = requests.get("https://labs.bible.org/api/?passage=" + passage)
-    response = str(r.content, "utf-8")
-    if not response.startswith("<b>"):
-        return "Please enter a valid passage!"
-    else:
-        clean_text = re.sub(remove_html, "", response)
-        return clean_text
-
+    params = {
+        'passage':passage,
+        'formatting':'plain',
+        'type':'json'
+    }
+    r = requests.get("https://labs.bible.org/api", params=params)
+    try:
+        response = r.json()[0]
+    except:
+        return "Something went wrong, either you entered an invalid passage or the API is down."
+    book = response['bookname']
+    ch = response['chapter']
+    ver = response['verse']
+    txt = response['text']
+    return "\x02{} {}:{}\x02 {}".format(book, ch, ver, txt)

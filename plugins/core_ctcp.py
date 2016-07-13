e3713e2d4b9604c45f41e444b9d3b4de486ecb44
Dabo Ross <daboross@daboross.net>
2016-07-13 14:29:29 -0700

    Provide a bit more of a robust fix for #167!

--- plugins/core_ctcp.py
+++ plugins/core_ctcp.py
@@ -10,19 +10,19 @@ from cloudbot.event import EventType
 @asyncio.coroutine
 @hook.event(EventType.other)
 def ctcp_version(notice, irc_ctcp_text):
-    if irc_ctcp_text is not None and irc_ctcp_text.lower() == "version":
+    if irc_ctcp_text is not None and irc_ctcp_text.lower().strip() == "version":
         notice("\x01VERSION: CloudBot {} - https://git.io/CloudBot".format(cloudbot.__version__))
 
 
 @asyncio.coroutine
 @hook.event(EventType.other)
 def ctcp_ping(notice, irc_ctcp_text):
-    if irc_ctcp_text is not None and irc_ctcp_text.lower() == "ping":
+    if irc_ctcp_text is not None and irc_ctcp_text.lower().strip() == "ping":
         notice('\x01PING: PONG')
 
 
 @asyncio.coroutine
 @hook.event(EventType.other)
 def ctcp_time(notice, irc_ctcp_text):
-    if irc_ctcp_text is not None and irc_ctcp_text.lower() == "time":
+    if irc_ctcp_text is not None and irc_ctcp_text.lower().strip() == "time":
         notice('\x01TIME: The time is: {}'.format(time.strftime("%r", time.localtime())))

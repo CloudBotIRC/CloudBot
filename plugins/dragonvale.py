46f17a873499f80d592860ed52615ce4f4a3b26b
Andrew Edwards <andrewthomasedwards@gmail.com>
2016-07-26 23:07:36 -0400

    add calculator to list dragons based on incubation times.

--- plugins/dragonvale.py
+++ plugins/dragonvale.py
@@ -1,9 +1,13 @@
 import requests
 import re
 
+from bs4 import BeautifulSoup
 from cloudbot import hook
+from cloudbot.util.timeparse import time_parse
 
 search_url = "http://dragonvale.wikia.com/api/v1/Search/list"
+
+egg_calc_url = "http://www.dragonvalebreedingguide.com/dragonvale-calculator"
 
 def striphtml(data):
     string = re.compile(r'<.*?>')
@@ -24,3 +28,31 @@ def dragonsearch(text):
     data = r.json()["items"][0]
     out = "\x02{}\x02 -- {}: {}".format(data["title"], striphtml(data["snippet"]).split("&hellip;")[0].strip(), data["url"])
     return out
+
+@hook.command("eggcalc", "dragoncalc", "dc")
+def egg_calculator(text):
+    """Parses dragonvalebreedingguide.com for a list of possible dragons based on the incubation time. Enter the time as 5 hours, 30 minutes. For upgraded incubation times put 'upgrade' at the front of the time length"""
+    time = ""
+    time2 = ""
+    if text.lower().startswith("upgrade"):
+        timer = text.replace("upgrade", "")
+        time2 = time_parse(timer.strip())
+        if not time2:
+            return "invalid time format"
+    else:
+        timer = text
+        time = time_parse(timer.strip())
+        if not time:
+            return "invalid time format"
+    params = {
+        'time': time,
+        'time2': time2,
+        'avail':1
+    }
+    r = requests.get(egg_calc_url, params=params, timeout=5)
+    soup = BeautifulSoup(r.text)
+    dragons = []
+    for line in soup.findAll('td', {'class':'views-field views-field-title'}):
+        dragons.append(line.text.replace("\n", "").strip())
+
+    return ", ".join(dragons)

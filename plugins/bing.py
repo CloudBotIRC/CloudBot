d9ddc1c8aa7c5749cc9328ea390387b15972cf13
Andrew Edwards <andrewthomasedwards@gmail.com>
2016-10-02 02:05:03 -0400

    add warning that the free bing api will go away in december.

--- plugins/bing.py
+++ plugins/bing.py
@@ -68,7 +68,7 @@ def bing(text, bot):
     desc = formatting.truncate(unescape(result["Description"]), 150)
     url = unescape(result["Url"])
 
-    return colors.parse('{} -- $(b){}$(b): "{}"'.format(url, title, desc))
+    return colors.parse('\x02Notice: The Bing API will stop working sometime in december because Microsoft is greedy as fuck and is removing the free search tier.\x02  {} -- $(b){}$(b): "{}"'.format(url, title, desc))
 
 
 @hook.command("bingimage", "bis")
@@ -123,4 +123,4 @@ def bingimage(text, bot):
     # join all the tags together in a comma separated string ("tag1, tag2, tag3")
     tag_text = ", ".join(tags)
 
-    return '{} ({})'.format(unescape(result["MediaUrl"]), tag_text)
+    return '\x02Notice: The Bing API will stop working sometime in december because Microsoft is greedy as fuck and is removing the free search tier.\x02 {} ({})'.format(unescape(result["MediaUrl"]), tag_text)

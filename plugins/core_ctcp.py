7c19300a704687b3dae3649e2b54d7b668e16504
linuxdemon1 <walter@walterbarnes.net>
2016-11-13 12:05:40 -0600

    Fixed the ctcp responses to use the standard format

--- plugins/core_ctcp.py
+++ plugins/core_ctcp.py
@@ -11,8 +11,8 @@ import cloudbot
 def ctcp_version(notice, irc_ctcp_text):
     if irc_ctcp_text:
         if irc_ctcp_text.startswith("VERSION"):
-            notice("\x01VERSION: gonzobot a fork of Cloudbot {} - https://snoonet.org/gonzobot".format(cloudbot.__version__))
+            notice("\x01VERSION gonzobot a fork of Cloudbot {} - https://snoonet.org/gonzobot\x01".format(cloudbot.__version__))
         elif irc_ctcp_text.startswith("PING"):
-            notice('\x01PING: PONG')
+            notice('\x01{}\x01'.format(irc_ctcp_text))  # Bot should return exactly what the user sends as the ping parameter
         elif irc_ctcp_text.startswith("TIME"):
-           notice('\x01TIME: The time is: {}'.format(time.strftime("%r", time.localtime())))
+           notice('\x01TIME {}\x01'.format(time.asctime()))  # General convention is to return the asc time
